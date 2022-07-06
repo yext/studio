106614f37754a4881a635479f2d1e270de9eeabb
Oliver Shi <oshi@yext.com>
2022-07-06 18:30:49 -0400

    remove extraneous index.css

--- studio/main.tsx
+++ studio/main.tsx
@@ -6,7 +6,7 @@ import { createRoot } from 'react-dom/client';
 //@ts-ignore TODO what's the best way to handle typescript and virtual modules? global.d.ts file?
 import virtualStudioContext from 'virtual:yext-studio';
 
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../dist/output.css';
 
 export function Main() {
