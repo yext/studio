bfb1cacffb645f079cba8fece1fdb3b50105f6ba
Oliver Shi <oshi@yext.com>
2022-07-13 23:02:22 -0400

    use the right type for drag event

--- studio/client/components/PropEditorList.tsx
+++ studio/client/components/PropEditorList.tsx
@@ -1,40 +1,39 @@
-import { DndContext, DragEvent } from '@dnd-kit/core'
+import { DndContext, DragEndEvent, DragOverEvent } from '@dnd-kit/core'
 import {
   arrayMove,
   SortableContext,
-  verticalListSortingStrategy,
-} from '@dnd-kit/sortable';
+  verticalListSortingStrategy
+} from '@dnd-kit/sortable'
 import DraggablePropEditor from './DraggablePropEditor'
-import { PageComponentsState } from '../../shared/models'
 import { useStudioContext } from './useStudioContext'
 import { PropState } from './PropEditor'
-import { useState } from 'react';
+import { useState } from 'react'
 
 export default function PropEditorList() {
   const { pageComponentsState, setPageComponentsState, componentsToPropShapes } = useStudioContext()
   const [listState, setListState] = useState(pageComponentsState)
   const items = listState.map(c => c.uuid)
 
-  function getUpdatedListState(activeId: string, overId: string) {
+  function getUpdatedListState(activeId: string, overId: string ) {
     const oldIndex = listState.findIndex(c => c.uuid === activeId)
     const newIndex = listState.findIndex(c => c.uuid === overId)
     const updatedListState = arrayMove(listState, oldIndex, newIndex)
     return updatedListState
   }
 
-  function handleDragEnd({ active, over }: DragEvent) {
+  function handleDragEnd({ active, over }: DragEndEvent) {
     if (!over?.id || active.id === over.id) {
       return
     }
-    const updatedListState = getUpdatedListState(active.id, over.id)
+    const updatedListState = getUpdatedListState(active.id as string, over.id as string)
     setListState(updatedListState)
   }
 
-  function handleDragOver({ active, over }: DragEvent) {
+  function handleDragOver({ active, over }: DragOverEvent) {
     if (!over?.id) {
       return
     }
-    const updatedListState = getUpdatedListState(active.id, over.id)
+    const updatedListState = getUpdatedListState(active.id as string, over.id as string)
     setPageComponentsState(updatedListState)
   }
 
