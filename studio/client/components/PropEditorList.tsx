7307a9ed153eec2a4199b62786a2c93506e63c7a
Oliver Shi <oshi@yext.com>
2022-07-13 22:51:10 -0400

    dragOver magic

--- studio/client/components/PropEditorList.tsx
+++ studio/client/components/PropEditorList.tsx
@@ -1,4 +1,4 @@
-import { DndContext } from '@dnd-kit/core'
+import { DndContext, DragEvent } from '@dnd-kit/core'
 import {
   arrayMove,
   SortableContext,
@@ -8,23 +8,44 @@ import DraggablePropEditor from './DraggablePropEditor'
 import { PageComponentsState } from '../../shared/models'
 import { useStudioContext } from './useStudioContext'
 import { PropState } from './PropEditor'
+import { useState } from 'react';
 
 export default function PropEditorList() {
   const { pageComponentsState, setPageComponentsState, componentsToPropShapes } = useStudioContext()
-  const items = pageComponentsState.map(c => c.uuid)
-  const setItems = updatedOrder => {
-    updatedOrder = typeof updatedOrder === 'function' ? updatedOrder(items) : updatedOrder
-    const updatedState = updatedOrder.map(uuid => {
-      return pageComponentsState.find(c => c.uuid === uuid)
-    }) as PageComponentsState
-    setPageComponentsState(updatedState)
+  const [listState, setListState] = useState(pageComponentsState)
+  const items = listState.map(c => c.uuid)
+
+  function getUpdatedListState({ active, over }: DragEvent) {
+    if (over?.id && active.id !== over.id) {
+      const oldIndex = listState.findIndex(c => c.uuid === active.id)
+      const newIndex = listState.findIndex(c => c.uuid === over.id)
+      const updatedListState = arrayMove(listState, oldIndex, newIndex)
+      return updatedListState
+    }
+  }
+
+  function handleDragEnd(event: DragEvent) {
+    const updatedListState = getUpdatedListState(event)
+    if (!updatedListState) {
+      return
+    }
+    setListState(updatedListState)
+  }
+
+  function handleDragOver(event: DragEvent) {
+    const updatedListState = getUpdatedListState(event)
+    if (!updatedListState) {
+      return
+    }
+    setPageComponentsState(updatedListState)
   }
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
+    <DndContext onDragEnd={handleDragEnd} onDragOver={handleDragOver}>
       <SortableContext items={items} strategy={verticalListSortingStrategy}>
-        {pageComponentsState.map(c => {
-          const uuid = c.uuid;
+        {items.map(uuid => {
+          const c = pageComponentsState.find(c => c.uuid === uuid)
+
           const setPropState = (val: PropState) => {
             const copy = [...pageComponentsState]
             const i = copy.findIndex(c => c.uuid === uuid)
@@ -49,14 +70,4 @@ export default function PropEditorList() {
       </SortableContext>
     </DndContext>
   )
-
-  function handleDragEnd({ active, over }) {
-    if (active.id !== over.id) {
-      setItems(items => {
-        const oldIndex = items.indexOf(active.id)
-        const newIndex = items.indexOf(over.id)
-        return arrayMove(items, oldIndex, newIndex)
-      })
-    }
-  }
 }
