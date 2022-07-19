69c8ee18e71a90a3a8e28961e7de7210d0115b15
Alex Taing <59857107+alextaing@users.noreply.github.com>
2022-07-19 09:04:03 -0400

    Add right click context menu to editor  (#7)
    Users can now right click added components in the LHS editor to show a custom context menu with a delete option.
    
    J=SLAP-2243
    TEST=manual
    
    Test that on right click, a custom context menu is displayed with a delete option.  When the delete option is pressed, the context menu goes away, and the component is deleted.  On clicking the `update` button, the prop is removed from the `index.tsx` file.

--- studio/client/components/DraggablePropEditor.tsx
+++ studio/client/components/DraggablePropEditor.tsx
@@ -1,6 +1,9 @@
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import PropEditor, { PropEditorProps } from './PropEditor'
+import { ControlledMenu, MenuItem, useMenuState } from '@szhsin/react-menu'
+import { useStudioContext } from './useStudioContext'
 
 interface DraggablePropEditorProps extends PropEditorProps {
   uuid: string
@@ -25,9 +28,58 @@ export default function DraggablePropEditor(props: DraggablePropEditorProps) {
     padding: '8px 4px'
   }
 
+  const { pageComponentsState, setPageComponentsState } = useStudioContext()
+  const [contextMenuAnchor, setContextMenuAnchor] = useState({ x: 0, y: 0 })
+  const [contextMenuProps, toggleContextMenu] = useMenuState()
+  const ref = useRef<HTMLDivElement>(null)
+
+  const handleContextMenu = useCallback((e) => {
+    e.preventDefault()
+    setContextMenuAnchor({ x: e.pageX, y: e.pageY })
+    toggleContextMenu(true)
+  }, [setContextMenuAnchor, toggleContextMenu])
+
+  useEffect(() => {
+    const element: HTMLDivElement | null = ref.current
+    if (element) {
+      element.addEventListener(
+        'contextmenu',
+        handleContextMenu
+      )
+    }
+    return (() => {
+      if (element) {
+        element.removeEventListener(
+          'contextmenu',
+          handleContextMenu
+        )
+      }
+    })
+  }, [handleContextMenu])
+
+  function deleteComponent() {
+    setPageComponentsState(pageComponentsState.filter((element) => {
+      return element.uuid !== props.uuid
+    }))
+  }
+
   return (
-    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
-      <PropEditor {...props}/>
+    <div ref={ref}>
+      <ControlledMenu
+        {...contextMenuProps}
+        anchorPoint={contextMenuAnchor}
+        onClose={() => toggleContextMenu(false)}
+      >
+        <MenuItem
+          onClick={deleteComponent}
+          className='px-3 py-1 rounded-md bg-white select-none cursor-pointer'
+        >
+          Delete
+        </MenuItem>
+      </ControlledMenu>
+      <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+        <PropEditor {...props} />
+      </div>
     </div>
   )
 }
