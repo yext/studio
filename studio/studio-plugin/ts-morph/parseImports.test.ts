0bc7c6744147dc07a7f612fa2be4d1218b3c52d9
Oliver Shi <oshi@yext.com>
2022-07-19 14:15:47 -0400

    refactor parsePropInterface to use generic helper

--- studio/studio-plugin/ts-morph/parseImports.test.ts
+++ studio/studio-plugin/ts-morph/parseImports.test.ts
@@ -1,19 +1,21 @@
 // import getRootPath from '../getRootPath';
-import parseImports from './parseImports';
+import parseImports from './parseImports'
+import getRootPath from '../getRootPath'
 
 jest.mock('../getRootPath')
 
 it('works for ColorProp use case', () => {
-  const imports = parseImports('components/Banner.tsx')
+  const imports = parseImports(getRootPath('components/Banner.tsx'))
   expect(imports).toEqual({
-    './SpecialProps': ['ColorProp']
+    './SpecialProps': ['ColorProp'],
+    '../../../types': ['HexColor']
   })
-});
+})
 
 it('can handle multiple exports, named + default exports, and renamed exports', () => {
-  const imports = parseImports('testPage.tsx')
+  const imports = parseImports(getRootPath('testPage.tsx'))
   expect(imports).toEqual({
     './components/Banner': ['BannerProps', 'RenamedImportedClassNames', 'Banner'],
     './components/SpecialProps': ['ColorProp']
   })
-});
+})
