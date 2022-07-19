89284edfd21f9509365502328203046959d1906b
Oliver Shi <oshi@yext.com>
2022-07-19 14:58:59 -0400

    refactor parsePropInterface to use generic helper (#9)
    separates `parseInterfaceDeclaration` from `parsePropInterface`, and also refactors it
    add github workflow to run unit tests

--- studio/studio-plugin/ts-morph/common.ts
+++ studio/studio-plugin/ts-morph/common.ts
@@ -1,6 +1,10 @@
-import { JsxOpeningElement, JsxSelfClosingElement, SourceFile, ts } from 'ts-morph'
+import { InterfaceDeclaration, JsxOpeningElement, JsxSelfClosingElement, SourceFile, ts } from 'ts-morph'
 import { JsxEmit } from 'typescript'
 import prettier from 'prettier'
+import { TSPropShape, TSPropType } from '../../shared/models'
+import { specialTypesArray } from '../../types'
+import parseImports from './parseImports'
+import { resolve } from 'path'
 
 export function getComponentNodes(sourceFile: SourceFile): (JsxOpeningElement | JsxSelfClosingElement)[] {
   const nodes = sourceFile
@@ -18,7 +22,7 @@ export const tsCompilerOptions = {
 }
 
 export function getComponentName(n: JsxOpeningElement | JsxSelfClosingElement): string {
-  const componentName =  n.getFirstDescendantByKindOrThrow(ts.SyntaxKind.Identifier).getText()
+  const componentName = n.getFirstDescendantByKindOrThrow(ts.SyntaxKind.Identifier).getText()
   return componentName;
 }
 
@@ -29,4 +33,52 @@ export function prettify(code: string) {
     singleQuote: true,
     jsxSingleQuote: true
   })
+}
+
+export function parseInterfaceDeclaration(propsInterface: InterfaceDeclaration, filePath: string) {
+  const structure = propsInterface.getStructure()
+  const properties = structure.properties ?? []
+  const props: TSPropShape = {}
+
+  let imports: Record<string, string[]>
+  properties.forEach(p => {
+    const jsdoc = p.docs?.map(doc => typeof doc === 'string' ? doc : doc.description).join('\n')
+
+    if (isPropType(p.type)) {
+      if (['string', 'number', 'boolean'].includes(p.type) || validateProp(p.type)) {
+        props[p.name] = {
+          type: p.type,
+          ...(jsdoc && { doc: jsdoc })
+        }
+      }
+    } else {
+      console.error(`Prop type ${p.type} is not recognized. Skipping gracefully.`)
+    }
+  })
+  return props
+
+  function validateProp(type: TSPropType): boolean {
+    if (!imports) {
+      imports = parseImports(filePath)
+    }
+
+    if (['string', 'boolean', 'number'].includes(type)) {
+      return true
+    }
+    const isValidProp = !!Object.entries(imports).find(([path, names]) => {
+      if (names.some(name => name === type)) {
+        return resolve(filePath, '..', path) === resolve(__dirname, '../../types')
+      }
+      return false
+    })
+    if (!isValidProp) {
+      console.error(`Skipping prop type ${type} because it was not imported from the Studio's types.ts.`)
+    }
+    return isValidProp
+  }
+
+  function isPropType(type: unknown): type is TSPropType {
+    const types = ['string', 'number', 'boolean'].concat(specialTypesArray)
+    return types.some(t => t === type)
+  }
 }
