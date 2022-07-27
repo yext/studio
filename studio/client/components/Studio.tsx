2925e2297eaf5a94ef33255922053dacd7b1e7c7
Kwun Ying (Juliann) Zhou <kzhou@yext.com>
2022-07-27 15:27:00 -0400

    use same url for tab switching

--- studio/client/components/Studio.tsx
+++ studio/client/components/Studio.tsx
@@ -4,12 +4,6 @@ import PagePreview from './PagePreview'
 import { PageComponentsState, ModuleNameToComponentMetadata } from '../../shared/models'
 import { StudioContext } from './useStudioContext'
 import { PageEditor } from './PageEditor'
-import {
-  BrowserRouter,
-  Route,
-  Routes
-} from 'react-router-dom'
-import { Navbar } from './Navbar'
 
 export interface StudioProps {
   siteSettings: SiteSettingsProps,
@@ -20,23 +14,28 @@ export interface StudioProps {
   }
 }
 
+enum Tab {
+  PageEditor = 'Page Editor',
+  SiteSettings = 'Site Settings'
+}
+
 export default function Studio(props: StudioProps) {
   const { componentsOnPage, moduleNameToComponentMetadata, siteSettings } = props
   const [pageComponentsState, setPageComponentsState] = useState(componentsOnPage.index)
   const value = { moduleNameToComponentMetadata, pageComponentsState, setPageComponentsState }
+  const [currentTab, setTab] = useState(Tab.PageEditor)
 
   return (
     <StudioContext.Provider value={value}>
       <div className='min-h-screen h-full w-screen flex flex-row'>
         <div className='w-2/5 bg-slate-500 flex flex-col'>
           <h1 className='text-3xl text-white'>Yext Studio</h1>
-          <BrowserRouter>
-            <Navbar />
-            <Routes>
-              <Route path='/studio/client' element={<PageEditor />} />
-              <Route path='/studio/client/SiteSettings' element={<SiteSettings {...siteSettings} />} />
-            </Routes>
-          </BrowserRouter>
+          <nav className='bg-gray-800 flex flex-row'>
+            <button onClick={() => { setTab(Tab.PageEditor) }} className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" hover:underline p-2'>Page Editor</button>
+            <div className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'></div>
+            <button onClick={() => { setTab(Tab.SiteSettings) }} className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" hover:underline p-2'>Site Settings</button>
+          </nav>
+          {currentTab === Tab.PageEditor ? <PageEditor /> : <SiteSettings {...siteSettings}/>}
         </div>
         <PagePreview />
       </div>
