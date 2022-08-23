cba2640b8081d2dc9ca3082c73efb2e536c11787
Oliver Shi <oshi@yext.com>
2022-08-23 10:53:19 -0400

    auto generate stream config (#22)
    This PR adds the ability to auto generate the stream config when writing to a page file.
    We do not support accessing the stream document using bracket notation, and only support dot notation.
    I.e. we support `document.id` but not `document['id']` or `document["id"]`. We may need to add bracket notation in the future to support fields with things like periods and dashes in their names. (We may also not need to, since afaik custom fields are formatted like `c_myCustomField`, and for general developer use this doesn't seem too important)
    
    We currently do not validate that the document fields used in studio actually exist. I plan to experiment with some kind of autocomplete/validation stuff in future PR. (I also need to address some organizational TODOs)
    
    TEST=manual,auto
    
    test that I can update the page file to point to various pieces of streams data and the config's fields will be automatically generated

---
+++ studio/studio-plugin/streams/getUpdatedStreamConfig.ts
@@ -0,0 +1,103 @@
+import { TemplateConfig } from '@yext/pages'
+import { ComponentState } from '../../shared/models'
+import { v1 } from 'uuid'
+import { STREAMS_TEMPLATE_REGEX } from '../../client/utils/getPreviewProps'
+import { PropTypes, StreamsDataExpression, StreamsStringExpression } from '../../types'
+
+const INFRA_STREAM_PROPERTIES = [
+  '__',
+  'businessId',
+  'id',
+  'key',
+  'locale',
+  'meta',
+  'siteDomain',
+  'siteId',
+  'siteInternalHostName',
+  'uid'
+]
+
+export default function getUpdatedStreamConfig(
+  componentsState: ComponentState[],
+  currentConfig: TemplateConfig = {}
+): TemplateConfig {
+  const streamValues = getStreamValues(componentsState)
+  const usedDocumentPaths = getUsedDocumentPaths(streamValues)
+  const fields = [...usedDocumentPaths]
+    .filter(documentPath => !INFRA_STREAM_PROPERTIES.includes(documentPath))
+    .map(documentPath => documentPath.split('document.')[1])
+
+  return {
+    ...currentConfig,
+    stream: {
+      $id: 'studio-stream-id_' + v1(),
+      filter: {},
+      localization: {
+        locales: ['en'],
+        primary: false,
+      },
+      ...currentConfig.stream,
+      fields
+    }
+  }
+}
+
+export function getUsedDocumentPaths(
+  streamValues: StreamValues
+): Set<StreamsDataExpression> {
+  const usedPaths: StreamsDataExpression[] = streamValues.documentPaths.map(d => d.split('[')[0] as StreamsDataExpression)
+  streamValues.templateStrings.forEach(val => {
+    const streamPaths = [...val.matchAll(STREAMS_TEMPLATE_REGEX)].map(m => m[1])
+    streamPaths.forEach(streamPath => {
+      // Streams configs fields do not allow specifying an index of a field.
+      // Cutting off at the first left bracket also lets use sidestep bracket object property notation,
+      // which we don't support.
+      const streamPathTruncatedAtBracket = streamPath.split('[')[0]
+      if (!isStreamsDataExpression(streamPathTruncatedAtBracket)) {
+        throw new Error(
+          `Error when parsing stream config, document path "${streamPath}" does not start with a "document." prefix`)
+      }
+      usedPaths.push(streamPathTruncatedAtBracket)
+    })
+  })
+  return new Set(usedPaths)
+}
+
+type StreamValues = {
+  documentPaths: StreamsDataExpression[],
+  templateStrings: Exclude<StreamsStringExpression, StreamsDataExpression>[]
+}
+
+export function getStreamValues(
+  componentsState: ComponentState[]
+): StreamValues {
+  const valuesAccumulator: StreamValues = {
+    documentPaths: [],
+    templateStrings: []
+  }
+
+  componentsState.forEach(({ props, moduleName }) => {
+    if (moduleName === 'builtIn') {
+      return
+    }
+    Object.keys(props).forEach(propName => {
+      const { type, value } = props[propName]
+      if (type === PropTypes.StreamsData) {
+        valuesAccumulator.documentPaths.push(value)
+      } else if (type === PropTypes.StreamsString) {
+        if (isStreamsDataExpression(value)) {
+          valuesAccumulator.documentPaths.push(value)
+        } else {
+          valuesAccumulator.templateStrings.push(value)
+        }
+      }
+    })
+  })
+  return valuesAccumulator
+}
+
+function isStreamsDataExpression(
+  value: string
+): value is StreamsDataExpression {
+  return value.startsWith('document.')
+}
