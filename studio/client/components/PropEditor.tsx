7d422eff1a6747c92eda770f84cfce86ec027283
Yen Truong <36055303+yen-tt@users.noreply.github.com>
2022-08-23 10:42:13 -0400

    use generic InputProp for all the different prop types in PropEditor (#23)
    Consolidate NumProp, StrProp, HexColorProp, etc to use a generic `InputProp` 
    
    J=SLAP-2334
    TEST=manual
    
    run `npm run dev`, make changes to props and saved. See that there's no error encountered.

--- studio/client/components/PropEditor.tsx
+++ studio/client/components/PropEditor.tsx
@@ -1,5 +1,6 @@
+import { CSSProperties } from 'react'
 import { PropState, ComponentMetadata } from '../../shared/models'
-import { HexColor, PropTypes, StreamsDataExpression, StreamsStringExpression } from '../../types'
+import { PropTypes } from '../../types'
 import kgLogoUrl from '../images/kg-logo.jpeg'
 
 export interface PropEditorProps {
@@ -12,7 +13,7 @@ export default function PropEditor({
   propState,
   setPropState,
   componentMetadata
-}: PropEditorProps) {
+}: PropEditorProps): JSX.Element | null {
   if (!componentMetadata || !componentMetadata.propShape) {
     console.error('Error rendering prop editor for', propState)
     return null
@@ -36,7 +37,7 @@ export default function PropEditor({
           const value = propState[propName]?.value
           const key = propName + '-' + index
           const sharedProps = {
-            key: propName,
+            key,
             propName,
             propValue: value as any,
             propDoc,
@@ -44,17 +45,17 @@ export default function PropEditor({
           }
           switch (type) {
             case PropTypes.boolean:
-              return <BoolProp {...sharedProps} key={key}/>
+              return <InputProp {...sharedProps} type='checkbox'/>
             case PropTypes.string:
-              return <StrProp {...sharedProps} key={key}/>
+              return <InputProp {...sharedProps}/>
             case PropTypes.number:
-              return <NumProp {...sharedProps} key={key}/>
+              return <InputProp {...sharedProps} type='number'/>
             case PropTypes.HexColor:
-              return <HexColorProp {...sharedProps} key={key}/>
+              return <InputProp {...sharedProps} type='color' defaultValue='#ffffff'/>
             case PropTypes.StreamsString:
-              return <StreamsStringProp {...sharedProps} key={key} />
+              return <InputProp {...sharedProps} img={<KGLogo style={{ filter: 'sepia(100%) saturate(300%) brightness(70%) hue-rotate(80deg)' }} />}/>
             case PropTypes.StreamsData:
-              return <StreamsDataProp {...sharedProps} key={key} />
+              return <InputProp {...sharedProps} img={<KGLogo />}/>
             default:
               console.error('Unknown prop type', type, 'for propName', propName, 'in propState', propState)
               return null
@@ -65,69 +66,42 @@ export default function PropEditor({
   )
 }
 
-function BoolProp(props: {
+function InputProp<T extends string | number | boolean>(props: {
   propName: string,
-  propValue: boolean,
+  propValue: T,
+  defaultValue?: T,
   propDoc?: string,
-  onChange: (val: boolean) => void
-}) {
+  type?: string,
+  img?: JSX.Element,
+  onChange: (val: T) => void
+}): JSX.Element {
+  const { propName, propValue, propDoc, onChange, type = 'text', defaultValue, img } = props
   return (
     <div className='flex'>
-      <label className='peer label'>{props.propName}:</label>
-      {props.propDoc && <ToolTip message={props.propDoc}/>}
-      <input className='checkbox' type='checkbox' onChange={e => props.onChange(e.target.checked)} checked={props.propValue ?? false}/>
-    </div>
-  )
-}
-
-function StrProp(props: {
-  propName: string,
-  propValue: string,
-  propDoc?: string,
-  onChange: (val: string) => void
-}) {
-  return (
-    <div className='flex'>
-      <label className='peer label'>{props.propName}:</label>
-      {props.propDoc && <ToolTip message={props.propDoc}/>}
-      <input className='input-sm' onChange={e => props.onChange(e.target.value)} value={props.propValue ?? ''}/>
-    </div>
-  )
-}
-
-function NumProp(props: {
-  propName: string,
-  propValue: number,
-  propDoc?: string,
-  onChange: (val: number) => void
-}) {
-  return (
-    <div className='flex'>
-      <label className='peer label'>{props.propName}:</label>
-      {props.propDoc && <ToolTip message={props.propDoc}/>}
-      <input className='input-sm' type="number" onChange={e => props.onChange(parseFloat(e.target.value))} value={props.propValue ?? ''}/>
-    </div>
-  )
-}
-
-function HexColorProp(props: {
-  propName: string,
-  propValue: HexColor,
-  propDoc?: string,
-  onChange: (val: HexColor) => void
-}) {
-  return (
-    <div className='flex'>
-      <label className='peer label'>{props.propName}:</label>
-      {props.propDoc && <ToolTip message={props.propDoc}/>}
-      <input type='color' className='input-sm' onChange={e => props.onChange(e.target.value as HexColor)} value={props.propValue ?? '#ffffff'}/>
+      <label className='peer label'>{propName}:</label>
+      {propDoc && <ToolTip message={propDoc}/>}
+      {type === 'checkbox'
+        ? <input
+          className='checkbox'
+          type='checkbox'
+          onChange={e => onChange(e.target.checked as T)}
+          checked={propValue as boolean ?? defaultValue}
+        />
+        : <input
+          className='input-sm'
+          type={type}
+          onChange={e => onChange(e.target.value as T)}
+          value={propValue as string | number ?? defaultValue}
+        />
+      }
+      {img}
     </div>
   )
 }
 
 function ToolTip(props: {
   message: string
-}) {
+}): JSX.Element {
   return (
     <div className='invisible peer-hover:visible'>
       <div className='absolute z-10 whitespace-nowrap rounded shadow-lg p-3 text-sm bg-gray-600 text-white'>{props.message}</div>
@@ -135,46 +109,11 @@ function ToolTip(props: {
   )
 }
 
-function StreamsStringProp(props: {
-  propName: string,
-  propValue: StreamsStringExpression,
-  propDoc?: string,
-  onChange: (val: StreamsStringExpression) => void
-}) {
-  return (
-    <div className='flex'>
-      <label className='peer label'>{props.propName}:</label>
-      {props.propDoc && <ToolTip message={props.propDoc}/>}
-      <input
-        className='input-sm'
-        onChange={e => props.onChange(e.target.value as StreamsStringExpression)}
-        value={props.propValue ?? ''}
-      />
-      <img
-        src={kgLogoUrl}
-        alt='this input uses streams'
-        className='h-8'
-        style={{ filter: 'sepia(100%) saturate(300%) brightness(70%) hue-rotate(80deg)' }}
-      />
-    </div>
-  )
-}
-function StreamsDataProp(props: {
-  propName: string,
-  propValue: StreamsDataExpression,
-  propDoc?: string,
-  onChange: (val: StreamsDataExpression) => void
-}) {
-  return (
-    <div className='flex'>
-      <label className='peer label'>{props.propName}:</label>
-      {props.propDoc && <ToolTip message={props.propDoc}/>}
-      <input
-        className='input-sm'
-        onChange={e => props.onChange(e.target.value as StreamsDataExpression)}
-        value={props.propValue ?? ''}
-      />
-      <img src={kgLogoUrl} alt='this input uses streams' className='h-8'/>
-    </div>
-  )
+function KGLogo({ style }: { style?: CSSProperties }): JSX.Element {
+  return <img
+    src={kgLogoUrl}
+    alt='this input uses streams'
+    className='h-8'
+    style={style}
+  />
 }
