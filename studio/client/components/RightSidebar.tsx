18af6c0329abf738e4e4881d8741ee4133549e83
Oliver Shi <oshi@yext.com>
2022-09-06 11:24:55 -0400

    trim

--- studio/client/components/RightSidebar.tsx
+++ studio/client/components/RightSidebar.tsx
@@ -9,13 +9,14 @@ enum Tab {
 
 export default function RightSidebar(): JSX.Element {
   const [currentTab, setTab] = useState(Tab.PageEditor)
+  const tabClassName = 'bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" hover:underline p-2'
 
   return (
     <div className='w-2/5 flex-grow bg-slate-500'>
       <nav className='bg-gray-800 flex flex-row'>
-        <button onClick={() => { setTab(Tab.PageEditor) }} className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" hover:underline p-2'>Page Editor</button>
+        <button onClick={() => setTab(Tab.PageEditor)} className={tabClassName}>Page Editor</button>
         <div className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'></div>
-        <button onClick={() => { setTab(Tab.SiteSettings) }} className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" hover:underline p-2'>Site Settings</button>
+        <button onClick={() => setTab(Tab.SiteSettings)} className={tabClassName}>Site Settings</button>
       </nav>
       {renderTab(currentTab)}
     </div>
