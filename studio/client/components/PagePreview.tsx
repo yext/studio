e6f646ffac333ea5f35e83e48eaeb57535923b39
Oliver Shi <oshi@yext.com>
2022-09-09 10:48:49 -0400

    combine!

--- studio/client/components/PagePreview.tsx
+++ studio/client/components/PagePreview.tsx
@@ -1,5 +1,4 @@
-import React, { FunctionComponent, useEffect, useCallback, useState, useMemo, createElement } from 'react'
-import { useRef } from 'react'
+import React, { FunctionComponent, useEffect, useCallback, useState, useMemo, createElement, useRef } from 'react'
 import { ModuleNameToComponentMetadata, PageState, ComponentState, ComponentMetadata } from '../../shared/models'
 import { useStudioContext } from './useStudioContext'
 import getPreviewProps from '../utils/getPreviewProps'
