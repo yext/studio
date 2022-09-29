815a6ed8d0a469887ed8e827c704196986d567e9
Yen Truong <ytruong@yext.com>
2022-09-29 15:58:35 -0400

    docs

--- studio/shared/StudioNpmComponentPlugin.ts
+++ studio/shared/StudioNpmComponentPlugin.ts
@@ -20,14 +20,15 @@ export interface StudioNpmComponentPlugin {
   /** List of components available to use by Studio. */
   exports: (string | ComponentExportConfig)[],
   /**
-   * By default, during development, Vite will crawl through source code to detect dependencies
-   * that need to be be converted to ESM during pre-bundling step since Vite's dev serves all
-   * code as native ESM.
+   * By default, during development, Vite will crawl through source code to detect
+   * dependencies that need to be be converted to ESM during pre-bundling step since
+   * Vite's dev serves all code as native ESM.
    *
-   * For dynamic import of NPM modules, CommonJS or UMD dependencies may not be discoverable
-   * by Vite during pre-bundling step. As such, use "nonEsmDeps" option to include any CommonJS
-   * or UMD dependencies. This array will be included as part of Vite's config "optimizedDeps.include"
-   * to force the listed dependencies to be pre-bundled.
+   * For dynamic import of NPM modules, CommonJS or UMD dependencies may not be
+   * discoverable by Vite during pre-bundling step. As such, use "nonEsmDeps" option
+   * to include any CommonJS or UMD dependencies. This array will be included as part
+   * of Vite's config "optimizedDeps.include" to force the listed dependencies to be
+   * pre-bundled.
    */
   nonEsmDeps?: string[]
 }
