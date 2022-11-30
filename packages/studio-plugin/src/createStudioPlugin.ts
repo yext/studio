6c4d92fc874118ce422198f80a9868f1e19cfcf0
Oliver Shi <oshi@yext.com>
2022-11-30 16:57:43 -0500

    before prettier

---
+++ packages/studio-plugin/src/createStudioPlugin.ts
@@ -0,0 +1,34 @@
+import { ConfigEnv, Plugin } from 'vite'
+import openBrowser from 'react-dev-utils/openBrowser.js'
+
+/**
+ * Handles server-client communication.
+ *
+ * This includes providing a vite virtual module so that server side data can be passed to the front end
+ * for the initial load, and messaging using the vite HMR API.
+ */
+ export default function createStudioPlugin(args: ConfigEnv): Plugin {
+  const virtualModuleId = 'virtual:yext-studio'
+  const resolvedVirtualModuleId = '\0' + virtualModuleId
+
+  const ctx = 'dummy studio data!'
+
+  return {
+    name: 'yext-studio-vite-plugin',
+    async buildStart() {
+      if (args.mode === 'development' && args.command === 'serve') {
+        openBrowser('http://localhost:5173/')
+      }
+    },
+    resolveId(id) {
+      if (id === virtualModuleId) {
+        return resolvedVirtualModuleId
+      }
+    },
+    load(id) {
+      if (id === resolvedVirtualModuleId) {
+        return `export default ${JSON.stringify(ctx)}`
+      }
+    }
+  }
+}
--- packages/studio/src/main.tsx
+++ packages/studio/src/main.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+import virtualStudioContext from 'virtual:yext-studio'
+
+console.log("virtual studio context =", virtualStudioContext)
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
