d683b3ae0efffb7b4c13607447b420eb1c2648a1
Oliver Shi <oshi@yext.com>
2022-12-01 15:32:15 -0500

    Scaffold studio-plugin package (#58)
    Adds packages/studio-plugin, moves some shared dev deps into the top level, adds a simple vite plugin that opens a browser on startup and provides some dummy virtual data.
    
    J=SLAP-2504
    TEST=manual

--- packages/studio/src/main.tsx
+++ packages/studio/src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import virtualStudioContext from "virtual:yext-studio";
+
+console.log("virtual studio context =", virtualStudioContext);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
--- packages/studio/vite.config.ts
+++ packages/studio/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig } from "vite";
+import createStudioPlugin from "@yext/studio-plugin";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((args: ConfigEnv) => ({
   root: __dirname,
-  plugins: [react()],
-});
+  plugins: [react(), createStudioPlugin(args)],
+}));
