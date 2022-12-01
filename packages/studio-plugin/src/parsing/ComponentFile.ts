976f8afc089c4f24797593fa30d501624eba8cb5
Oliver Shi <oshi@yext.com>
2022-12-01 15:35:50 -0500

    wip

---
+++ packages/studio-plugin/src/parsing/ComponentFile.ts
@@ -0,0 +1,5 @@
+import StudioSourceFile from './StudioSourceFile'
+
+export default class ComponentFile {
+  constructor(private studioSourceFile: StudioSourceFile) {}
+}
---
+++ packages/studio-plugin/src/parsing/StudioSourceFile.ts
@@ -0,0 +1,5 @@
+import { SourceFile } from 'ts-morph'
+
+export default class StudioSourceFile {
+  constructor(private sourceFile: SourceFile) {}
+}
