d676c1bc2703a53ac0122811c05650fb6208c005
Oliver Shi <oshi@yext.com>
2022-12-02 16:37:16 -0500

    wip

--- packages/studio-plugin/src/parsing/ComponentFile.ts
+++ packages/studio-plugin/src/parsing/ComponentFile.ts
@@ -1,5 +1,139 @@
 import StudioSourceFile from './StudioSourceFile'
 
+enum FileMetadataKind {
+  Component = 'componentMetadata',
+}
+
+type ComponentMetadata = {
+  kind: FileMetadataKind.Component,
+  initialProps?: PropValues,
+  propShape?: PropShape,
+  acceptsChildren?: boolean
+}
+
+type PropShape = Omit<{
+  [propName: string]: PropMetadata
+}, SpecialReactProps> & {
+    [propName in SpecialReactProps]?: never
+  }
+
+enum SpecialReactProps {
+  Children = 'children'
+}
+
+type PropMetadata = {
+  type: PropValueType,
+  doc?: string
+}
+
+enum PropStateKinds {
+  Literal = 'literal'
+}
+
+enum PropValueType {
+  number = 'number',
+  string = 'string',
+  boolean = 'boolean',
+  ReactNode = 'ReactNode',
+  HexColor = 'HexColor',
+}
+
+type PropValues = {
+  [propName: string]: PropVal
+}
+type PropVal = LiteralProp
+
+type LiteralProp = {
+  kind: PropStateKinds.Literal
+} & (NumberProp | StringProp | BooleanProp | HexColorProp)
+
+type NumberProp = {
+  valueType: PropValueType.number,
+  value: number
+}
+type StringProp = {
+  valueType: PropValueType.string,
+  value: string
+}
+type BooleanProp = {
+  valueType: PropValueType.boolean,
+  value: boolean
+}
+type HexColorProp = {
+  valueType: PropValueType.HexColor,
+  value: string
+}
+
+import path from 'path'
+
 export default class ComponentFile {
-  constructor(private studioSourceFile: StudioSourceFile) {}
+  private studioSourceFile: StudioSourceFile
+  private componentName: string
+
+  constructor(filepath: string) {
+    this.componentName = path.basename(filepath, '.tsx')
+    this.studioSourceFile = new StudioSourceFile(filepath)
+  }
+
+
+  getComponentMetadata(): ComponentMetadata {
+    const propsInterface =
+      this.studioSourceFile.getInterfaceByName(`${this.componentName}Props`)
+    const properties = propsInterface.getStructure().properties
+    if (!properties) {
+      return {
+        kind: FileMetadataKind.Component,
+      }
+    }
+
+    const studioImports = this.studioSourceFile.getImportsFromStudio()
+    const propShape: PropShape = {}
+    const initialProps = this.studioSourceFile.getExportedObjectLiteralOrThrow
+    let acceptsChildren = false
+
+    properties.forEach(p => {
+      const { name: propName, type } = p
+      if (typeof type !== 'string') {
+        console.error("Unable to parse prop:", propName, "for props interface for", this.componentName)
+        return
+      }
+      if (propName === SpecialReactProps.Children) {
+        acceptsChildren = propName === SpecialReactProps.Children
+        return
+      }
+      if (!isPropValueType(type)) {
+        console.error("Unrecognized prop type", type, "in props interface for", this.componentName)
+        return
+      }
+      if (!isPrimitiveProp(type) && !studioImports.includes(type)) {
+        console.error("Missing import for", type, "in props interface for", this.componentName)
+      }
+
+      const jsdoc = p.docs?.map(doc => typeof doc === 'string' ? doc : doc.description).join('\n')
+      propShape[p.name] = {
+        type,
+        ...(jsdoc && { doc: jsdoc })
+      }
+    })
+
+    return {
+      kind: FileMetadataKind.Component,
+      propShape,
+      acceptsChildren,
+      initialProps
+    }
+  }
+}
+
+function isPrimitiveProp(propValueType: PropValueType) {
+  return [
+    PropValueType.boolean,
+    PropValueType.string,
+    PropValueType.number
+  ].includes(propValueType)
+}
+
+function isPropValueType(type: string): type is PropValueType {
+  const propTypes = Object.values(PropValueType)
+  return propTypes.includes(type as PropValueType)
 }
--- packages/studio-plugin/src/parsing/StudioSourceFile.ts
+++ packages/studio-plugin/src/parsing/StudioSourceFile.ts
@@ -1,5 +1,65 @@
-import { SourceFile } from 'ts-morph'
+import { ts, InterfaceDeclaration, Project, SourceFile, SyntaxKind, JSDocableNodeStructure, TypedNodeStructure, PropertyNamedNodeStructure, PropertySignatureStructure, OptionalKind, ObjectLiteralExpression } from 'ts-morph'
+import typescript from 'typescript'
+
+// 'typescript' is a CommonJS module, which may not support all module.exports as named exports
+const { JsxEmit } = typescript
+
+const project = new Project({
+  compilerOptions: {
+    jsx: JsxEmit.ReactJSX
+  }
+})
+
 
 export default class StudioSourceFile {
-  constructor(private sourceFile: SourceFile) {}
+  private sourceFile: SourceFile
+
+  constructor(filepath: string) {
+    if (!project.getSourceFile(filepath)) {
+      project.addSourceFileAtPath(filepath)
+    }
+    this.sourceFile = project.getSourceFileOrThrow(filepath)
+  }
+
+  getInterfaceByName(interfaceName: string): InterfaceDeclaration {
+    return this.sourceFile.getInterfaceOrThrow(interfaceName);
+  }
+
+  getImportsFromStudio(): string[] {
+    return this.parseImports()['@yext/studio']
+  }
+
+  private parseImports(): Record<string, string[]> {
+    const importPathToImportNames: Record<string, string[]> = {}
+
+    this.sourceFile.getDescendantsOfKind(SyntaxKind.ImportDeclaration).forEach(importDeclaration => {
+      const importClause = importDeclaration.getFirstDescendantByKind(SyntaxKind.ImportClause)
+      //  Ignore imports like `import 'index.css'` which lack an import clause
+      if (!importClause) {
+        return
+      }
+      const defaultImport: string | undefined = importClause.getDefaultImport()?.getText()
+      const namedImports: string[] = importClause.getNamedImports()
+        .map(n => n.compilerNode.name.escapedText.toString())
+      const importPath: string = importDeclaration.getModuleSpecifierValue()
+      if (!importPathToImportNames[importPath]) {
+        importPathToImportNames[importPath] = []
+      }
+      importPathToImportNames[importPath].push(...namedImports)
+      defaultImport && importPathToImportNames[importPath].push(defaultImport)
+    })
+    return importPathToImportNames
+  }
+
+  getExportedObjectLiteralOrThrow(variableName: string): ObjectLiteralExpression {
+    const exportSymbols = this.sourceFile.getExportSymbols()
+    const propSymbol = exportSymbols.find(s => s.getEscapedName() === variableName)
+    const objectLiteralExp = propSymbol
+      ?.getValueDeclaration()
+      ?.getFirstDescendantByKind(ts.SyntaxKind.ObjectLiteralExpression)
+    if (!objectLiteralExp) {
+      throw new Error(`Could not find ObjectLiteralExpression for variable ${variableName}`)
+    }
+    return objectLiteralExp
+  }
 }
