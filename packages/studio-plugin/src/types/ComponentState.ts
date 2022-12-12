ac4249d72c06887b6c43050c35d5661bf59d95b4
nmanu1 <nmanu@yext.com>
2022-12-12 09:38:52 -0500

    Remove ComponentState.ts

--- packages/studio-plugin/src/types/ComponentState.ts
+++
@@ -1,9 +0,0 @@
-import { PropValues } from "./PropValues";
-
-export type ComponentState = {
-  name: string;
-  props: PropValues;
-  uuid: string;
-  metadataUUID?: string;
-  parentUUID?: string;
-};
