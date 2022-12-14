5c8376fc195c9e4ac5c8769dd8acf1811b624e66
Oliver Shi <oshi@yext.com>
2022-12-14 11:12:31 -0500

    Revert "remove ParsingOrchestrator for now"
    This reverts commit 3e39a7c87b9ad1038604dcb4345ec1c89f0c4d71.

---
+++ packages/studio-plugin/tests/__fixtures__/ParsingOrchestrator/pages/basicPage.tsx
@@ -0,0 +1,10 @@
+import Card from "../components/Card";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Card text="first!" />
+      <Card />
+    </div>
+  );
+}
---
+++ packages/studio-plugin/tests/parsing/ParsingOrchestrator.test.ts
@@ -0,0 +1,77 @@
+import ParsingOrchestrator from '../../src/parsing/ParsingOrchestrator'
+import path from 'path'
+import getStudioPaths from '../../src/parsing/getStudioPaths';
+import { ComponentStateKind, FileMetadataKind } from '../../src';
+
+it('getComponentMetadata', () => {
+  const orchestrator = createOrchestrator()
+  expect(orchestrator.getComponentMetadata()).toEqual([
+    expect.objectContaining({ kind: FileMetadataKind.Component, propShape: expect.anything() }),
+    expect.objectContaining({ kind: FileMetadataKind.Component, propShape: expect.anything(), acceptsChildren: true })
+  ])
+})
+
+it('getModuleMetadata', () => {
+  const orchestrator = createOrchestrator()
+  expect(orchestrator.getModuleMetadata()).toEqual([
+    {
+      kind: FileMetadataKind.Module,
+      componentTree: [
+        expect.objectContaining({ componentName: 'NestedBanner' }),
+        expect.objectContaining({ componentName: 'Card' }),
+      ],
+      initialProps: expect.anything(),
+      propShape: expect.anything()
+    },
+    {
+      kind: FileMetadataKind.Module,
+      componentTree: [
+        expect.objectContaining({ kind: ComponentStateKind.Fragment }),
+        expect.objectContaining({ componentName: "BannerWithCard" }),
+        expect.objectContaining({ componentName: "BannerWithCard" }),
+      ],
+      propShape: expect.anything()
+    }
+  ])
+})
+
+it('getPages', () => {
+  const orchestrator = createOrchestrator()
+  expect(orchestrator.getPages()).toEqual({
+    basicPage: {
+      componentTree: [
+        expect.objectContaining({
+          componentName: 'div',
+          kind: ComponentStateKind.Standard
+        }),
+        expect.objectContaining({
+          componentName: "Card",
+          kind: ComponentStateKind.Standard
+        }),
+        expect.objectContaining({
+          componentName: "Card",
+          kind: ComponentStateKind.Standard
+        })
+      ],
+      cssImports: []
+    },
+    pageWithModules: {
+      componentTree: [
+        expect.objectContaining({
+          componentName: 'NestedBanner',
+          kind: ComponentStateKind.Standard
+        }),
+        expect.objectContaining({
+          componentName: 'NestedModule',
+          kind: ComponentStateKind.Module
+        })
+      ],
+      cssImports: []
+    }
+  })
+})
+
+function createOrchestrator() {
+  const studioPaths = getStudioPaths(path.resolve(__dirname, '../__fixtures__/ParsingOrchestrator'))
+  return new ParsingOrchestrator(studioPaths)
+}
