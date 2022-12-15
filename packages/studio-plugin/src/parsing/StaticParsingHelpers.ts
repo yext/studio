8a0b650399399929a5baf700034f8f243deff909
Oliver Shi <oshi@yext.com>
2022-12-15 10:57:43 -0500

    remove extra ElementType from merge

--- packages/studio-plugin/src/parsing/StaticParsingHelpers.ts
+++ packages/studio-plugin/src/parsing/StaticParsingHelpers.ts
@@ -356,9 +356,3 @@ export default class StaticParsingHelpers {
     );
   }
 }
-
-type ElementType<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<
-  infer ElementType
->
-  ? ElementType
-  : never;
