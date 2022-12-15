eac7045b8f6c932224f0608f5cf3bd3b16a974d6
Oliver Shi <oshi@yext.com>
2022-12-15 18:54:29 -0500

    wip upgrade deps see if prettier changed

--- packages/studio-plugin/src/index.ts
+++ packages/studio-plugin/src/index.ts
@@ -1,4 +1,18 @@
 import createStudioPlugin from "./createStudioPlugin";
 
 export default createStudioPlugin;
-export * from "./types";
+export {
+  PropShape,
+  PropValues,
+  PropValueKind,
+  PropValueType,
+  PropVal,
+  ComponentMetadata,
+  ModuleMetadata,
+  FileMetadata,
+  FileMetadataKind,
+  ComponentState,
+  PageState,
+  ComponentStateKind,
+  StudioData
+} from "./types";
--- packages/studio/vite.config.ts
+++ packages/studio/vite.config.ts
@@ -1,9 +1,12 @@
 import { ConfigEnv, defineConfig } from "vite";
 import createStudioPlugin from "@yext/studio-plugin";
 import react from "@vitejs/plugin-react";
+import rollupTypescript from '@rollup/plugin-typescript'
 
+console.log('starto!')
 // https://vitejs.dev/config/
 export default defineConfig((args: ConfigEnv) => ({
   root: __dirname,
-  plugins: [react(), createStudioPlugin(args)],
+  plugins: [react(), createStudioPlugin(args), rollupTypescript()],
+  // include: ['@yext/studio-plugin']
 }));
