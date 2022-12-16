7f937f09fa2f7990dbed97fa94fd628f5d6a5f0f
Oliver Shi <oshi@yext.com>
2022-12-15 19:00:59 -0500

    wip before trying pnpm

--- packages/studio/vite.config.ts
+++ packages/studio/vite.config.ts
@@ -1,12 +1,19 @@
 import { ConfigEnv, defineConfig } from "vite";
 import createStudioPlugin from "@yext/studio-plugin";
 import react from "@vitejs/plugin-react";
-import rollupTypescript from '@rollup/plugin-typescript'
+
+
 
 console.log('starto!')
 // https://vitejs.dev/config/
-export default defineConfig((args: ConfigEnv) => ({
-  root: __dirname,
-  plugins: [react(), createStudioPlugin(args), rollupTypescript()],
-  // include: ['@yext/studio-plugin']
-}));
+export default defineConfig(async (args: ConfigEnv) => {
+  const studioPluginModule = await import('@yext/studio-plugin')
+  console.log(studioPluginModule)
+  return {
+    root: __dirname,
+    plugins: [react(), createStudioPlugin(args)],
+    optimizeDeps: {
+      exclude: ['@yext/studio-plugin']
+    }
+  }
+});
