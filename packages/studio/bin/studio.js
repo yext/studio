88decbffbc12a6f060d3a98a22a175bae847418b
Oliver Shi <oshi@yext.com>
2022-12-15 20:00:33 -0500

    even more working now

--- packages/studio/bin/studio.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const { execSync } = require("child_process");
-const path = require("path");
-console.log('directly running js!')
-execSync(
-  "pnpm exec vite --config " + path.resolve(__dirname, "../vite.config.ts"),
-  { stdio: "inherit" }
-);
--- packages/studio/vite.config.ts
+++ packages/studio/vite.config.ts
@@ -2,8 +2,6 @@ import { ConfigEnv, defineConfig } from "vite";
 import createStudioPlugin from "@yext/studio-plugin";
 import react from "@vitejs/plugin-react";
 
-console.log('starto!')
-
 export default defineConfig((args: ConfigEnv) => {
   return {
     root: __dirname,
