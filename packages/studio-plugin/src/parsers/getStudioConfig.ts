9b9fd81931f12cce820b1386d1cc7a11cb405a91
Yen Truong <ytruong@yext.com>
2023-01-03 14:23:33 -0500

    json => ts

--- packages/studio-plugin/src/parsers/getStudioConfig.ts
+++ packages/studio-plugin/src/parsers/getStudioConfig.ts
@@ -4,14 +4,14 @@ import path from "path";
 
 /**
  * Given an absolute path to the user's project root folder, retrieve Studio's
- * configuration defined in "studioconfig.json" file, if exist.
+ * configuration defined in "studioconfig.ts" file, if exist.
  *
  * @param pathToProjectRoot - An absolute path to the project's root folder
  */
 export default async function getStudioConfig(
   pathToProjectRoot: string
 ): Promise<StudioConfig> {
-  const configFilepath = path.join(pathToProjectRoot, "studioconfig.json");
+  const configFilepath = path.join(pathToProjectRoot, "studioconfig.ts");
   if (!fs.existsSync(configFilepath)) {
     return {};
   }
