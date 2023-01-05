e0199a7fd3598edf714cb5da4da0407ddf60f059
Oliver Shi <oshi@yext.com>
2023-01-05 14:54:28 -0500

    try

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -5,7 +5,7 @@ import { devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './e2e',
+  testDir: './tests',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -35,7 +35,7 @@ const config: PlaywrightTestConfig = {
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'cd .. && npm run dev',
     port: 5173,
   },
 };
