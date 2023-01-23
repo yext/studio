0df0e087a849ea0cc59b4a8e4b64b7a805ade037
Oliver Shi <oshi@yext.com>
2023-01-23 17:21:12 -0500

    wip before tests

--- packages/studio/src/store/deleteModuleAction.ts
+++ packages/studio/src/store/deleteModuleAction.ts
@@ -7,6 +7,9 @@ import {
 } from "@yext/studio-plugin";
 import { v4 } from "uuid";
 
+/**
+ * getDeleteModuleAction is a factory method for the deleteModule action.
+ */
 export default function getDeleteModuleAction(
   get: () => StudioStore
 ): StudioStore["deleteModule"] {
@@ -15,30 +18,10 @@ export default function getDeleteModuleAction(
     store.pages.setActiveComponentUUID(undefined);
     const activePageState = store.pages.getActivePageState();
     if (activePageState) {
-      const updatedComponentTree =
-        activePageState?.componentTree.flatMap((componentStateToDetach) => {
-          const isModule =
-            componentStateToDetach.kind === ComponentStateKind.Module;
-          if (
-            !isModule ||
-            componentStateToDetach.metadataUUID !== metadata.metadataUUID
-          ) {
-            return componentStateToDetach;
-          }
-          const detachedTree =
-            ComponentTreeHelpers.mapComponentTreeParentsFirst<ComponentState>(
-              metadata.componentTree,
-              (child, parentValue) => {
-                return {
-                  ...child,
-                  uuid: v4(),
-                  parentUUID:
-                    parentValue?.uuid ?? componentStateToDetach.parentUUID,
-                };
-              }
-            );
-          return detachedTree;
-        }) ?? [];
+      const updatedComponentTree = detachModuleInstances(
+        metadata,
+        activePageState.componentTree
+      );
       store.pages.setActivePageState({
         ...activePageState,
         componentTree: updatedComponentTree,
@@ -46,4 +29,35 @@ export default function getDeleteModuleAction(
     }
     store.fileMetadatas.removeFileMetadata(metadata.metadataUUID);
   };
+}
+
+/**
+ * Loops through the given componentTree, and detaches all modules that are instances
+ * of the passed in ModuleMetadata.
+ */
+function detachModuleInstances(
+  metadata: ModuleMetadata,
+  componentTree: ComponentState[]
+) {
+  return componentTree.flatMap((componentStateToDetach) => {
+    const isModule = componentStateToDetach.kind === ComponentStateKind.Module;
+    if (
+      !isModule ||
+      componentStateToDetach.metadataUUID !== metadata.metadataUUID
+    ) {
+      return componentStateToDetach;
+    }
+    const detachedTree =
+      ComponentTreeHelpers.mapComponentTreeParentsFirst<ComponentState>(
+        metadata.componentTree,
+        (child, parentValue) => {
+          return {
+            ...child,
+            uuid: v4(),
+            parentUUID: parentValue?.uuid ?? componentStateToDetach.parentUUID,
+          };
+        }
+      );
+    return detachedTree;
+  });
 }
