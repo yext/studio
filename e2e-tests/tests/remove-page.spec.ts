665c32941c6ba09a7ff7ba593b9858ca440f74b2
Tristan Timblin <ttimblin@yext.com>
2023-01-30 11:24:07 -0500

    remove initial logic adding a page

--- e2e-tests/tests/remove-page.spec.ts
+++ e2e-tests/tests/remove-page.spec.ts
@@ -3,24 +3,7 @@ import { test, expect } from "@playwright/test";
 test("can remove a page", async ({ page }) => {
   await page.goto("./");
 
-  const newPageInTree = page.getByText("MyNewPage");
-  await expect.poll(() => newPageInTree.count()).toBe(0);
-
-  await page
-    .getByRole("button", {
-      exact: true,
-      name: "Add Page",
-    })
-    .click();
-  await page.getByRole("textbox").type("My New Page");
-  await page
-    .getByRole("dialog", {
-      exact: true,
-      name: "Add Page Modal",
-    })
-    .getByText("Save")
-    .click();
-
+  const newPageInTree = page.getByText("UniversalPage");
   await expect.poll(() => newPageInTree.count()).toBe(1);
 
   await page
