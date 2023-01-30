aae19e8d081cc6514147117a285b310c2d99428d
Tristan Timblin <ttimblin@yext.com>
2023-01-30 12:12:13 -0500

    add check for saving to file button

--- e2e-tests/tests/remove-page.spec.ts
+++ e2e-tests/tests/remove-page.spec.ts
@@ -4,7 +4,13 @@ test("can remove a page", async ({ page }) => {
   await page.goto("./");
 
   const newPageInTree = page.getByText("UniversalPage");
+  const saveToFileButton = page.getByRole("button", {
+    exact: true,
+    name: "Save to file",
+  });
+
   await expect.poll(() => newPageInTree.count()).toBe(1);
+  await expect(saveToFileButton).toBeDisabled();
 
   await page
     .locator("li", {
@@ -18,4 +24,5 @@ test("can remove a page", async ({ page }) => {
   await page.getByText("Delete", { exact: true }).click();
 
   await expect.poll(() => newPageInTree.count()).toBe(0);
+  await expect(saveToFileButton).toBeEnabled();
 });
