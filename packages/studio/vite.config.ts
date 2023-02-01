9942bdc4e325fc0428d6b90a4df78abb37ba05c2
nmanu1 <nmanu@yext.com>
2023-02-01 14:19:34 -0500

    wip

--- packages/studio/vite.config.ts
+++ packages/studio/vite.config.ts
@@ -1,4 +1,10 @@
-import { ConfigEnv, defineConfig, PluginOption, UserConfig } from "vite";
+import {
+  ConfigEnv,
+  defineConfig,
+  PluginOption,
+  searchForWorkspaceRoot,
+  UserConfig,
+} from "vite";
 import createStudioPlugin from "@yext/studio-plugin";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
@@ -6,6 +12,11 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig(async (args: ConfigEnv): Promise<UserConfig> => {
   return {
     root: __dirname,
+    server: {
+      fs: {
+        allow: [searchForWorkspaceRoot(process.cwd()), process.cwd()],
+      },
+    },
     build: {
       rollupOptions: {
         input: ["index.html", "src/store/useStudioStore.ts"],
