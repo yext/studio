31e772c4b0513206917cea4c93d907c2ba0c2cb9
Oliver Shi <oshi@yext.com>
2023-02-01 17:21:48 -0500

    revert page changes

--- apps/test-site/src/pages/UniversalPage.tsx
+++ apps/test-site/src/pages/UniversalPage.tsx
@@ -1,9 +1,17 @@
+import { AceComponent } from "@yext/sample-component";
 import Banner from "../components/Banner";
+import Button from "../components/Button";
+import Container from "../components/Container";
 import ContainerWithButtons from "../modules/ContainerWithButtons";
 
 export default function UniversalPage() {
   return (
     <div>
+      <Banner />
+      <Container>
+        <Button />
+      </Container>
+      <AceComponent text="ace" />
       <ContainerWithButtons />
       <Banner nestedProp={{ egg: "eggyweggy" }} />
     </div>
