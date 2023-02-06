de24932b524def81ca3f0e5e1653cb86ba24eb05
Oliver Shi <oshi@yext.com>
2023-02-06 16:29:01 -0500

    opt

--- apps/test-site/src/components/Button.tsx
+++ apps/test-site/src/components/Button.tsx
@@ -1,5 +1,5 @@
 export interface ButtonProps {
-  bgColors: "bg-indigo-100" | "bg-blue-100" | "bg-red-100";
+  bgColors?: "bg-indigo-100" | "bg-blue-100" | "bg-red-100";
 }
 
 export default function Button(props: ButtonProps) {
