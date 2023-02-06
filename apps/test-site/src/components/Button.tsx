51df1b1cdd3f5f1a745a6437d492dc755947078e
Oliver Shi <oshi@yext.com>
2023-02-06 17:34:42 -0500

    string union supp

--- apps/test-site/src/components/Button.tsx
+++ apps/test-site/src/components/Button.tsx
@@ -1,7 +1,7 @@
 export interface ButtonProps {
-  bgColors?: "bg-indigo-100" | "bg-blue-100" | "bg-red-100";
+  bgColor?: "bg-indigo-100" | "bg-blue-100" | "bg-red-100";
 }
 
 export default function Button(props: ButtonProps) {
-  return <button className={props.bgColors}>Press me!</button>;
+  return <button className={props.bgColor}>Press me!</button>;
 }
