2d4730debb7f2071a4b66ccdbf90361d8f854ed0
Oliver Shi <oshi@yext.com>
2023-02-07 12:02:55 -0500

    fix jsdoc

--- packages/studio/src/store/models/slices/StudioConfigSlice.ts
+++ packages/studio/src/store/models/slices/StudioConfigSlice.ts
@@ -1,8 +1,7 @@
 import { UserPaths } from "@yext/studio-plugin";
 
 /**
- * Keeps track of the state of the application at the time of the last commit.
- * Currently only siteSettings is tracked here.
+ * A slice for containing data sourced from the user's studio.config.ts.
  */
 export default interface StudioConfigSlice {
   paths: UserPaths;
