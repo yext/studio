de11d0a881bb60e306e7896014253c75243b8861
Oliver Shi <oshi@yext.com>
2023-02-08 16:35:10 -0500

    Yext Studio Commit

--- apps/test-site/src/pages/UniversalPage.tsx
+++ apps/test-site/src/pages/UniversalPage.tsx
@@ -1,14 +1,25 @@
+import { AceComponent } from "@yext/sample-component";
 import Banner from "../components/Banner";
 import Container from "../components/Container";
 import ContainerWithButtons from "../modules/ContainerWithButtons";
 
 export default function UniversalPage() {
   return (
-    <div>
-      <Banner />
-      <Container />
-      <ContainerWithButtons />
-      <Banner nestedProp={{ egg: "eggyweggy" }} />
-    </div>
+    <>
+      <div>
+        <Banner />
+        <Container />
+        <ContainerWithButtons />
+        <Banner
+          nestedProp={{ egg: "eggyweggy" }}
+          bgColor="#FFFFFF"
+          bool={false}
+          num={0}
+          title=""
+        />
+      </div>
+      <AceComponent text="ace" />
+      <AceComponent text="ace" />
+    </>
   );
 }
