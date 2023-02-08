f64dc7626814bb3e7ffe680448782795903d1633
Oliver Shi <oshi@yext.com>
2023-02-08 16:33:46 -0500

    Yext Studio Commit

--- apps/test-site/src/pages/UniversalPage.tsx
+++ apps/test-site/src/pages/UniversalPage.tsx
@@ -1,6 +1,5 @@
 import { AceComponent } from "@yext/sample-component";
 import Banner from "../components/Banner";
-import Button from "../components/Button";
 import Container from "../components/Container";
 import ContainerWithButtons from "../modules/ContainerWithButtons";
 
@@ -8,9 +7,7 @@ export default function UniversalPage() {
   return (
     <div>
       <Banner />
-      <Container>
-        <Button bgColor="bg-red-100" />
-      </Container>
+      <Container />
       <AceComponent text="ace" />
       <ContainerWithButtons />
       <Banner nestedProp={{ egg: "eggyweggy" }} />
