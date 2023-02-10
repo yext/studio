8a320a92584bc974b37fbbf110e770e0473ba6f2
Oliver Shi <oshi@yext.com>
2023-02-10 15:40:18 -0500

    intermediary commit

--- packages/studio/src/components/DeployButton.tsx
+++ packages/studio/src/components/DeployButton.tsx
@@ -6,7 +6,7 @@ import { Tooltip } from "react-tooltip";
 
 const tooltipAnchorID = "YextStudio-deployButton";
 
-console.log('!!!!!\n\n', gitData);
+console.log('!\n\n', gitData);
 
 /**
  * Renders a button for saving, committing, and pushing changes..
