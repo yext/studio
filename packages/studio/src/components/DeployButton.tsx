a3abb75c5d1e79986ff1330170ad3da3270a38bd
Oliver Shi <oshi@yext.com>
2023-02-10 14:49:57 -0500

    Yext Studio Commit

--- packages/studio/src/components/DeployButton.tsx
+++ packages/studio/src/components/DeployButton.tsx
@@ -1,6 +1,7 @@
 import useStudioStore from "../store/useStudioStore";
 import { useCallback, useState } from "react";
 import gitData from "virtual:yext-studio-git-data";
+import useHasChanges from "../hooks/useHasChanges";
 
 console.log(gitData);
 
@@ -10,6 +11,7 @@ console.log(gitData);
 export default function DeployButton() {
   const deploy = useStudioStore((store) => store.actions.deploy);
   const [deployInProgress, setDeployInProgress] = useState(false);
+  const hasChanges = useHasChanges();
 
   const handleClick = useCallback(async () => {
     setDeployInProgress(true);
@@ -17,11 +19,13 @@ export default function DeployButton() {
     setDeployInProgress(false);
   }, [deploy, setDeployInProgress]);
 
+  const isDisabled = !hasChanges && (deployInProgress || !gitData.canPush.status)
+
   return (
     <button
       className="ml-4 py-1 px-3 text-white rounded-md disabled:bg-gray-400 bg-blue-600 hover:bg-blue-500"
       onClick={handleClick}
-      disabled={deployInProgress && gitData.canPush.status}
+      disabled={isDisabled}
       aria-label="Deploy Changes to Repository"
     >
       Deploy
