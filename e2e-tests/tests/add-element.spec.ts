3887ec350f4721d741d9ba173a81b99d23134fd1
Oliver Shi <oshi@yext.com>
2023-02-23 14:19:08 -0500

    can get rid of expect.poll)

--- e2e-tests/tests/add-element.spec.ts
+++ e2e-tests/tests/add-element.spec.ts
@@ -3,7 +3,7 @@ import { studioTest } from "./infra/studioTest.js";
 
 studioTest("can add a container component", async ({ page, studioPage }) => {
   const previews = page.getByText("I'm a container:");
-  await expect.poll(() => previews.count()).toBe(1);
+  await expect(previews).toHaveCount(1);
   await studioPage.addElement("Container", "Containers");
-  await expect.poll(() => previews.count()).toBe(2);
+  await expect(previews).toHaveCount(2);
 });
--- e2e-tests/tests/remove-page.spec.ts
+++ e2e-tests/tests/remove-page.spec.ts
@@ -3,7 +3,7 @@ import { studioTest } from "./infra/studioTest.js";
 
 studioTest("can remove a page", async ({ page, studioPage }) => {
   const pageInTree = page.getByText("UniversalPage");
-  await expect.poll(() => pageInTree.count()).toBe(1);
+  await expect(pageInTree).toHaveCount(1);
   await studioPage.removePage("UniversalPage");
-  await expect.poll(() => pageInTree.count()).toBe(0);
+  await expect(pageInTree).toHaveCount(0);
 });
