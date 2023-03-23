a5c856a493f2fa3786522ecc0bcf1bcb123fae79
Oliver Shi <oshi@yext.com>
2023-03-23 13:32:22 -0400

    Add FieldPicker and FieldDropdown (#150)
    This PR creates the FieldPicker and FieldDropdown components for inserting entity fields into expressions.
    It differs from the mocks in that it doesn't open additional dropdowns to the right, which would wind up offscreen.
    <img width="265" alt="image" src="https://user-images.githubusercontent.com/23005393/226682173-c09b32db-6691-4ebe-bb9f-2b6600cec9b0.png">
    
    J=SLAP-2603
    TEST=manual,auto
    
    can click on the field picker and add a string field to an expression
    can use the field picker in a repeater list expression input and set the field

---
+++ packages/studio/src/components/FieldPicker/FieldPicker.tsx
@@ -0,0 +1,87 @@
+import { useCallback, useRef, useState, MouseEvent } from "react";
+import FieldDropdown from "./FieldDropdown";
+import { ReactComponent as EmbedIcon } from "../../icons/embed.svg";
+import useRootClose from "@restart/ui/useRootClose";
+import filterStreamDocument from "../../utils/filterStreamDocument";
+
+/**
+ * An icon that when clicked on, opens up a dropdown for selecting
+ * stream document fields.
+ */
+export default function FieldPicker({
+  handleFieldSelection,
+  streamDocument,
+  fieldType = "string",
+}: {
+  handleFieldSelection: (fieldId: string) => void;
+  streamDocument: Record<string, unknown>;
+  fieldType: "string" | "array";
+}) {
+  const [expandedFieldId, setExpandedFieldId] = useState<string>();
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useRootClose(containerRef, () => {
+    setExpandedFieldId(undefined);
+  });
+
+  const fieldPickerIsClosed = expandedFieldId === undefined;
+  const togglePicker = useCallback(
+    (e: MouseEvent<SVGSVGElement>) => {
+      e.preventDefault();
+      if (fieldPickerIsClosed) {
+        setExpandedFieldId("");
+      } else {
+        setExpandedFieldId(undefined);
+      }
+    },
+    [fieldPickerIsClosed]
+  );
+
+  const filteredDocument = filterStreamDocument(fieldType, streamDocument);
+
+  const handleFieldDropdownSelection = useCallback(
+    (fieldId: string) => {
+      handleFieldSelection(`document.${fieldId}`);
+      setExpandedFieldId(undefined);
+    },
+    [handleFieldSelection]
+  );
+
+  const handleNestedObjectSelection = useCallback(
+    (fieldId: string) => {
+      if (expandedFieldId !== fieldId) {
+        setExpandedFieldId(fieldId);
+      } else {
+        const parentFieldId = fieldId.substring(0, fieldId.lastIndexOf("."));
+        setExpandedFieldId(parentFieldId);
+      }
+    },
+    [expandedFieldId]
+  );
+
+  const isExpandedFieldId = useCallback(
+    (fieldId: string) => {
+      return !!expandedFieldId?.startsWith(fieldId);
+    },
+    [expandedFieldId]
+  );
+
+  return (
+    <div ref={containerRef}>
+      <EmbedIcon
+        role="button"
+        onClick={togglePicker}
+        className="hover:opacity-100 opacity-50 cursor-pointer"
+        aria-label="Toggle field picker"
+      />
+      {!fieldPickerIsClosed && (
+        <FieldDropdown
+          fieldIdToValue={filteredDocument}
+          handleFieldSelection={handleFieldDropdownSelection}
+          handleNestedObjectSelection={handleNestedObjectSelection}
+          isExpandedFieldId={isExpandedFieldId}
+        />
+      )}
+    </div>
+  );
+}
---
+++ packages/studio/tests/components/FieldPicker/FieldPicker.test.tsx
@@ -0,0 +1,105 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import FieldPicker from "../../../src/components/FieldPicker/FieldPicker";
+
+const streamDocument = {
+  __: {
+    templateType: "JS",
+  },
+  address: {
+    city: "Chicago",
+    postalCode: "60661",
+    region: "IL",
+  },
+  bool: false,
+  name: "benny",
+  profile: {
+    desc: "lorem ipsum quicksum pipsum",
+  },
+  c_myCustomField: "my custom field",
+};
+
+it("can display available string fields and objects containing string fields", async () => {
+  renderFieldPicker();
+  expect(screen.queryByText(/\w/)).toBeNull();
+  await userEvent.click(
+    screen.getByRole("button", { name: "Toggle field picker" })
+  );
+  expect(screen.getByText("Address")).toBeDefined();
+  expect(screen.getByText("Name")).toBeDefined();
+  expect(screen.getByText("Profile")).toBeDefined();
+  expect(screen.queryByText("__")).toBeNull();
+  expect(screen.queryByText("Bool")).toBeNull();
+  expect(screen.getByText("My Custom Field")).toBeDefined();
+});
+
+it("can display nested fields", async () => {
+  renderFieldPicker();
+  await userEvent.click(
+    screen.getByRole("button", { name: "Toggle field picker" })
+  );
+  expect(screen.queryByText("City")).toBeNull();
+  await userEvent.click(screen.getByText("Address"));
+  expect(screen.getByText("City")).toBeDefined();
+});
+
+it("can select nested fields", async () => {
+  const handleFieldSelection = jest.fn();
+  renderFieldPicker(handleFieldSelection);
+  await userEvent.click(
+    screen.getByRole("button", { name: "Toggle field picker" })
+  );
+  await userEvent.click(screen.getByText("Address"));
+  expect(handleFieldSelection).toHaveBeenCalledTimes(0);
+  await userEvent.click(screen.getByText("Postal Code"));
+  expect(handleFieldSelection).toHaveBeenCalledTimes(1);
+  expect(handleFieldSelection).toHaveBeenCalledWith(
+    "document.address.postalCode"
+  );
+});
+
+it("can close the field picker", async () => {
+  renderFieldPicker();
+  await userEvent.click(
+    screen.getByRole("button", { name: "Toggle field picker" })
+  );
+  expect(screen.getAllByText(/\w/)).toBeDefined();
+  await userEvent.click(
+    screen.getByRole("button", { name: "Toggle field picker" })
+  );
+  expect(screen.queryByText(/\w/)).toBeNull();
+});
+
+it("can collapse a section", async () => {
+  renderFieldPicker();
+  await userEvent.click(
+    screen.getByRole("button", { name: "Toggle field picker" })
+  );
+  await userEvent.click(screen.getByText("Address"));
+  expect(screen.getByText("City")).toBeDefined();
+  await userEvent.click(screen.getByText("Address"));
+  expect(screen.queryByText("City")).toBeNull();
+});
+
+it("opening one section will collapse unrelated ones", async () => {
+  renderFieldPicker();
+  await userEvent.click(
+    screen.getByRole("button", { name: "Toggle field picker" })
+  );
+  await userEvent.click(screen.getByText("Address"));
+  expect(screen.getByText("City")).toBeDefined();
+  expect(screen.queryByText("Desc")).toBeNull();
+  await userEvent.click(screen.getByText("Profile"));
+  expect(screen.queryByText("City")).toBeNull();
+  expect(screen.getByText("Desc")).toBeDefined();
+});
+
+function renderFieldPicker(handleFieldSelection = jest.fn()) {
+  render(
+    <FieldPicker
+      streamDocument={streamDocument}
+      handleFieldSelection={handleFieldSelection}
+      fieldType="string"
+    />
+  );
+}
