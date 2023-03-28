52dd1351217032e0438e767091bde57611a96945
nmanu1 <nmanu@yext.com>
2023-03-28 15:55:01 -0400

    Add new fixture

---
+++ e2e-tests/tests/__fixtures__/hmr-updated-page.tsx
@@ -0,0 +1,10 @@
+import Button from "../components/Button";
+
+export default function UniversalPage() {
+  return (
+    <>
+      <Button />
+      <Button />
+    </>
+  );
+}
--- e2e-tests/tests/hmr/update-page.spec.ts
+++ e2e-tests/tests/hmr/update-page.spec.ts
@@ -2,18 +2,21 @@ import { expect } from "@playwright/test";
 import { studioTest } from "../infra/studioTest.js";
 import fs from "fs";
 
-const pageWithAddedElement = fs.readFileSync(
-  "./tests/__fixtures__/add-element-page.tsx",
+const updatedPage = fs.readFileSync(
+  "./tests/__fixtures__/hmr-updated-page.tsx",
   "utf-8"
 );
 
 studioTest(
   "can update page file and see changes in UI via HMR",
   async ({ page, studioPage }) => {
-    const previews = page.getByText("I'm a container:");
-    await expect(previews).toHaveCount(1);
-    fs.writeFileSync("./src/pages/UniversalPage.tsx", pageWithAddedElement);
-    await expect(previews).toHaveCount(2);
+    const containerPreviews = page.getByText("I'm a container:");
+    await expect(containerPreviews).toHaveCount(1);
+    const buttonPreviews = page.getByText("Press me!");
+    await expect(buttonPreviews).toHaveCount(0);
+    fs.writeFileSync("./src/pages/UniversalPage.tsx", updatedPage);
+    await expect(containerPreviews).toHaveCount(0);
+    await expect(buttonPreviews).toHaveCount(2);
     await expect(studioPage.saveButton).toBeDisabled();
     await expect(page).toHaveScreenshot();
   }
