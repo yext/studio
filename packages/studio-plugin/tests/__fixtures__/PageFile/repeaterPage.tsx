dd58fe59ed94d1c294f1e79f3064595b544f219a
nmanu1 <nmanu@yext.com>
2023-04-04 07:09:56 -0400

    Add to .prettierignore and revert change

--- packages/studio-plugin/tests/__fixtures__/PageFile/repeaterPage.tsx
+++ packages/studio-plugin/tests/__fixtures__/PageFile/repeaterPage.tsx
@@ -4,9 +4,7 @@ import ComplexBanner from "../ComponentFile/ComplexBanner";
 export default function IndexPage({ document }: TemplateProps) {
   return (
     <>
-      {document.services.map((item, index) => (
-        <ComplexBanner key={index} />
-      ))}
+      {document.services.map((item, index) => <ComplexBanner key={index} />)}
     </>
   );
 }
