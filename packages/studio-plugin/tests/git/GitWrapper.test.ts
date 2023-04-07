c21f6f6528f687832838ea5e234520ff641386b7
Tom Meyer <tmeyer@yext.com>
2023-04-07 14:35:02 -0400

    Add additional test coverage for `GitWrapper`. (#162)
    J=SLAP-2671
    TEST=auto

--- packages/studio-plugin/tests/git/GitWrapper.test.ts
+++ packages/studio-plugin/tests/git/GitWrapper.test.ts
@@ -1,4 +1,4 @@
-import { simpleGit } from "simple-git";
+import { simpleGit, Response, RemoteWithRefs } from "simple-git";
 import GitWrapper from "../../src/git/GitWrapper";
 
 jest.mock("simple-git", () => {
@@ -8,21 +8,136 @@ jest.mock("simple-git", () => {
         add: jest.fn(),
         commit: jest.fn(),
         push: jest.fn(),
+        getRemotes: jest.fn(),
+        revparse: jest.fn(),
+        diff: jest.fn(),
       };
     },
   };
 });
-const mockedGitInstance = simpleGit();
-const gitAddSpy = jest.spyOn(mockedGitInstance, "add");
-const gitCommitSpy = jest.spyOn(mockedGitInstance, "commit");
-const gitPushSpy = jest.spyOn(mockedGitInstance, "push");
-
-const gitWrapper = new GitWrapper(mockedGitInstance);
 
 it("invokes Git commands correctly on deploy", async () => {
+  const mockedGitInstance = simpleGit();
+  const gitAddSpy = jest.spyOn(mockedGitInstance, "add");
+  const gitCommitSpy = jest.spyOn(mockedGitInstance, "commit");
+  const gitPushSpy = jest.spyOn(mockedGitInstance, "push");
+
+  const gitWrapper = new GitWrapper(mockedGitInstance);
+
   await gitWrapper.deploy();
 
   expect(gitAddSpy).toBeCalledWith("-A");
   expect(gitCommitSpy).toBeCalledWith("Yext Studio Commit");
   expect(gitPushSpy).toBeCalled();
 });
+
+describe("verifying canPush calculation", () => {
+  const mockedGitInstance = simpleGit();
+  const gitRemotesSpy = jest.spyOn(mockedGitInstance, "getRemotes");
+  const gitRevParseSpy = jest.spyOn(mockedGitInstance, "revparse");
+  const gitDiffSpy = jest.spyOn(mockedGitInstance, "diff");
+
+  const gitWrapper = new GitWrapper(mockedGitInstance);
+
+  it("handles no remotes correctly", async () => {
+    gitRemotesSpy.mockReturnValue(getResponseWithRemotes([]));
+    await gitWrapper.refreshData();
+
+    const expectedData = {
+      canPush: {
+        status: false,
+        reason: "No remotes found.",
+      },
+    };
+    expect(gitWrapper.getStoredData()).toEqual(expectedData);
+  });
+
+  it("handles multiple remotes correctly", async () => {
+    const refs = { fetch: "fetch", push: "push" };
+    const remotes: RemoteWithRefs[] = [
+      {
+        name: "remote 1",
+        refs,
+      },
+      {
+        name: "remote 2",
+        refs,
+      },
+    ];
+    gitRemotesSpy.mockReturnValue(getResponseWithRemotes(remotes));
+    await gitWrapper.refreshData();
+
+    const expectedData = {
+      canPush: {
+        status: false,
+        reason: "Multiple remotes found.",
+      },
+    };
+    expect(gitWrapper.getStoredData()).toEqual(expectedData);
+  });
+
+  it("handles no upstream branch correctly", async () => {
+    gitRemotesSpy.mockReturnValue(getDefaultResponseWithRemotes());
+    gitRevParseSpy.mockReturnValue(getResponseWithString(""));
+    await gitWrapper.refreshData();
+
+    const expectedData = {
+      canPush: {
+        status: false,
+        reason: "No upstream branch found",
+      },
+    };
+    expect(gitWrapper.getStoredData()).toEqual(expectedData);
+  });
+
+  it("handles no changes correctly", async () => {
+    gitRemotesSpy.mockReturnValue(getDefaultResponseWithRemotes());
+    gitRevParseSpy.mockReturnValue(getResponseWithString("branch"));
+    gitDiffSpy.mockReturnValue(getResponseWithString(""));
+    await gitWrapper.refreshData();
+
+    const expectedData = {
+      canPush: {
+        status: false,
+        reason: "No changes to push.",
+      },
+    };
+    expect(gitWrapper.getStoredData()).toEqual(expectedData);
+  });
+
+  it("returns status 'true' correctly", async () => {
+    gitRemotesSpy.mockReturnValue(getDefaultResponseWithRemotes());
+    gitRevParseSpy.mockReturnValue(getResponseWithString("branch"));
+    gitDiffSpy.mockReturnValue(getResponseWithString("Diff"));
+    await gitWrapper.refreshData();
+
+    const expectedData = {
+      canPush: {
+        status: true,
+      },
+    };
+    expect(gitWrapper.getStoredData()).toEqual(expectedData);
+  });
+});
+
+function getDefaultResponseWithRemotes(): Response<RemoteWithRefs[]> {
+  const refs = { fetch: "fetch", push: "push" };
+  const remotes: RemoteWithRefs[] = [
+    {
+      name: "remote 1",
+      refs,
+    },
+  ];
+
+  return getResponseWithRemotes(remotes);
+}
+
+function getResponseWithRemotes(
+  remotes: RemoteWithRefs[]
+): Response<RemoteWithRefs[]> {
+  return Promise.resolve(remotes) as Response<RemoteWithRefs[]>;
+}
+
+function getResponseWithString(value: string): Response<string> {
+  return Promise.resolve(value) as Response<string>;
+}
