6577f07532a851c431c6c62d83e3149e8f5b9fa4
Oliver Shi <oshi@yext.com>
2023-04-11 10:42:20 -0400

    update test

--- e2e-tests/tests/deploy.spec.ts
+++ e2e-tests/tests/deploy.spec.ts
@@ -1,13 +1,29 @@
 import { expect } from "@playwright/test";
 import { studioTest } from "./infra/studioTest.js";
+import simpleGit from "simple-git";
+const git = simpleGit();
 
 studioTest.use({
   createRemote: true,
 });
 
-studioTest("can deploy changes", async ({ page, studioPage }) => {
+studioTest.only("can deploy changes", async ({ page, studioPage }) => {
+  const startingRef = await git.revparse(["HEAD"]);
   await studioPage.addElement("Container", "Containers");
   await expect(page).toHaveScreenshot();
+
+  const numCommitsBeforeDeploy = await getNumCommitsFromRef(startingRef);
+  expect(numCommitsBeforeDeploy).toEqual(0);
   await studioPage.deployButton.click();
+
   await expect(page).toHaveScreenshot();
+  const numCommitsMade = await getNumCommitsFromRef(startingRef);
+  expect(numCommitsMade).toEqual(1);
+  const commitMsg = await git.raw(["show-branch", "--no-name", "HEAD"]);
+  expect(commitMsg).toEqual("Yext Studio Commit\n");
 });
+
+async function getNumCommitsFromRef(ref: string): Promise<number> {
+  const rawOutput = await git.raw(["rev-list", `${ref}..`, "--count"]);
+  return parseInt(rawOutput.trim());
+}
