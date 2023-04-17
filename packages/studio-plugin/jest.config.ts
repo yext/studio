358432d2a84c070848bec19121e1bc7a8a3c771e
Oliver Shi <oshi@yext.com>
2023-04-17 16:11:16 -0400

    use babel-jest

--- packages/studio-plugin/jest.config.ts
+++ packages/studio-plugin/jest.config.ts
@@ -6,8 +6,5 @@ const config: Config = {
   collectCoverageFrom: ["src/**", "!src/types/**", "!src/index.ts"],
   resetMocks: true,
   restoreMocks: true,
-  transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest"
-  }
 };
 export default config;
--- packages/studio/jest.config.ts
+++ packages/studio/jest.config.ts
@@ -10,19 +10,7 @@ const config: Config = {
   transformIgnorePatterns: ["node_modules/(?!react-tooltip/.*)"],
   setupFilesAfterEnv: ["<rootDir>/tests/__setup__/setup-env.ts"],
   transform: {
-    // "^.+\\.(t|j)sx?$": "babel-jest",
-    "^.+\\.(t|j)sx?$": [
-      "@swc/jest", {
-        sourceMaps: false,
-        jsc: {
-          transform: {
-            react: {
-              runtime: 'automatic',
-            },
-          },
-        },
-      }
-    ],
+    "^.+\\.(t|j)sx?$": "babel-jest",
     "\\.svg$": "<rootDir>/tests/__setup__/svgTransformer.cjs",
   },
 };
