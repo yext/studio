b521b9c4feabba6b200bdef10056f695bf36dbb4
nmanu1 <nmanu@yext.com>
2023-04-24 16:18:21 -0400

    Add doc

--- packages/studio/src/hooks/useFuncWithZundoBatching.tsx
+++ packages/studio/src/hooks/useFuncWithZundoBatching.tsx
@@ -2,6 +2,11 @@ import { useCallback, useMemo } from "react";
 import useTemporalStore from "../store/useTemporalStore";
 import { debounce } from "lodash";
 
+/**
+ * Updates a function so it doesn't trigger Zundo store updates until after a
+ * debounce period of 500ms. This results in state updates being batched for
+ * undo/redo.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useFuncWithZundoBatching<P extends Array<any>, R>(
   func: (...args: P) => R
