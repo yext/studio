a855c00bb9d61c041ed8c566bb9672aa845cf4a1
nmanu1 <88398086+nmanu1@users.noreply.github.com>
2023-04-26 17:06:07 -0400

    Add rearrange elements acceptance test (#175)
    Add a Playwright test for rearranging the components in a component tree.
    
    Playwright locators have a `dragTo` method, but it doesn't work in this case. We have to manually call the mouse up/down/move events because we need to be able to specify `steps` during `mouse.move`. This sends intermediate `mousemove` events during the move, which is needed for `@minoru/react-dnd-treeview` to register the dragging behavior properly.
    
    J=SLAP-2482
    TEST=auto

---
+++ e2e-tests/tests/rearrange-elements.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from "@playwright/test";
+import { studioTest } from "./infra/studioTest.js";
+import fs from "fs";
+
+const expectedPage = fs.readFileSync(
+  "./tests/__fixtures__/rearrange-elements-expected-page.tsx",
+  "utf-8"
+);
+
+studioTest("can rearrange elements in tree", async ({ page, studioPage }) => {
+  const banner = page.getByText("Banner");
+  const div = page.getByRole("list").filter({ hasText: "div" });
+  const divBox = await div.boundingBox();
+
+  await banner.hover();
+  await page.mouse.down();
+  divBox && (await page.mouse.move(divBox.x, divBox.y, { steps: 20 }));
+  await page.mouse.up();
+  await expect(page).toHaveScreenshot();
+
+  await studioPage.saveButton.click();
+  await expect("./src/pages/UniversalPage.tsx").toHaveContents(expectedPage);
+  await expect(page).toHaveScreenshot();
+});
