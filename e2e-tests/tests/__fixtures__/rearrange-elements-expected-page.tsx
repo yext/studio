66fbc828c8ee1d8135b88dab3d623d8565c358f8
nmanu1 <nmanu@yext.com>
2023-04-26 12:42:41 -0400

    Add rearrange elements acceptance test

---
+++ e2e-tests/tests/__fixtures__/rearrange-elements-expected-page.tsx
@@ -0,0 +1,13 @@
+import Banner from "../components/Banner";
+import Container from "../components/Container";
+
+export default function UniversalPage() {
+  return (
+    <>
+      <Banner />
+      <div>
+        <Container />
+      </div>
+    </>
+  );
+}
