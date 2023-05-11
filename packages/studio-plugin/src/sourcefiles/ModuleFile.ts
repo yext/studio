ca41a9f410539d95652335367702e0bf74e9b4d0
Oliver Shi <oshi@yext.com>
2023-05-11 12:22:30 -0400

    remove log

--- packages/studio-plugin/src/sourcefiles/ModuleFile.ts
+++ packages/studio-plugin/src/sourcefiles/ModuleFile.ts
@@ -84,7 +84,6 @@ export default class ModuleFile {
       };
     });
     const propArgs = ModuleFile.calcPropArgs(moduleMetadata.componentTree);
-    console.log("propargs", propArgs);
     this.reactComponentFileWriter.updateFile({
       componentTree: moduleMetadata.componentTree,
       moduleMetadata,
