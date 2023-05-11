3e31d14c0b8838c00d5b31ba293e2075f65e8bb3
Oliver Shi <oshi@yext.com>
2023-05-11 16:29:32 -0400

    revert timeout change

--- e2e-tests/tests/infra/ToastActionButton.ts
+++ e2e-tests/tests/infra/ToastActionButton.ts
@@ -21,7 +21,7 @@ export default class ToastActionButton {
   async click() {
     await this.button.click();
     await expect(() => expect(this.successToast).toHaveCount(1)).toPass({
-      timeout: 10_000,
+      timeout: 25_000,
     });
     await this.successToast.click();
     await expect(this.successToast).toHaveCount(0);
