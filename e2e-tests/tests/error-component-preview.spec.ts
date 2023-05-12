a99d3722299f244f981f16e0ee8eb131c4742294
Oliver Shi <oshi@yext.com>
2023-05-12 12:29:26 -0400

    first

--- e2e-tests/tests/error-component-preview.spec.ts
+++ e2e-tests/tests/error-component-preview.spec.ts
@@ -5,7 +5,7 @@ studioTest.only("can create new nested modules", async ({ page, studioPage }) =>
   await studioPage.switchPage('ErrorComponentPreviews');
   await expect(page).toHaveScreenshot();
 
-  await page.getByText('We will still try our best to render this component even with a parsing error.').hover();
+  await page.getByText('We will still try our best to render this component even with a parsing error.').first().hover();
   await expect(page).toHaveScreenshot();
 
   await studioPage.setActiveComponent('ErrorComponent');
