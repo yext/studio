db8007b62d741d35054fd7a3bb5bcdce111d57c6
Oliver Shi <oshi@yext.com>
2023-05-12 14:04:57 -0400

    use abs path for screenshots checkout at end

--- e2e-tests/tests/infra/setupGitBranch.ts
+++ e2e-tests/tests/infra/setupGitBranch.ts
@@ -97,5 +97,5 @@ async function restoreGitState(
   if (uncommittedChanges) {
     await git.reset(["HEAD^"]);
   }
-  await git.raw(["checkout", postTestRef, "--", "__screenshots__"]);
+  await git.raw(["checkout", postTestRef, "--", screenshotsPath]);
 }
