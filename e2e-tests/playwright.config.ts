731602f07699c05c11625a071719bc5377f02acf
Oliver Shi <oshi@yext.com>
2023-05-12 12:47:35 -0400

    drop thresholds more

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -32,8 +32,8 @@ const config: PlaywrightTestConfig = {
      */
     timeout: 5000,
     toHaveScreenshot: {
-      threshold: 0.005,
-      maxDiffPixelRatio: 0.005,
+      threshold: 0.001,
+      maxDiffPixelRatio: 0.001,
     },
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
