147ff32df7e30ec313cf579656488369b8db568f
Oliver Shi <oshi@yext.com>
2023-05-12 08:00:30 -0400

    remove only

--- e2e-tests/tests/rearrange-elements.spec.ts
+++ e2e-tests/tests/rearrange-elements.spec.ts
@@ -7,21 +7,18 @@ const expectedPage = fs.readFileSync(
   "utf-8"
 );
 
-studioTest.only(
-  "can rearrange elements in tree",
-  async ({ page, studioPage }) => {
-    const banner = page.getByText("Banner");
-    const div = page.getByRole("list").filter({ hasText: "div" });
-    const divBox = await div.boundingBox();
+studioTest("can rearrange elements in tree", async ({ page, studioPage }) => {
+  const banner = page.getByText("Banner");
+  const div = page.getByRole("list").filter({ hasText: "div" });
+  const divBox = await div.boundingBox();
 
-    await banner.hover();
-    await page.mouse.down();
-    divBox && (await page.mouse.move(divBox.x, divBox.y, { steps: 20 }));
-    await page.mouse.up();
-    await expect(page).toHaveScreenshot();
+  await banner.hover();
+  await page.mouse.down();
+  divBox && (await page.mouse.move(divBox.x, divBox.y, { steps: 20 }));
+  await page.mouse.up();
+  await expect(page).toHaveScreenshot();
 
-    await studioPage.saveButton.click();
-    await expect("./src/pages/UniversalPage.tsx").toHaveContents(expectedPage);
-    await expect(page).toHaveScreenshot();
-  }
-);
+  await studioPage.saveButton.click();
+  await expect("./src/pages/UniversalPage.tsx").toHaveContents(expectedPage);
+  await expect(page).toHaveScreenshot();
+});
