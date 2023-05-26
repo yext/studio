c020a03c93f2a0ade990f24c6e68cbabb55b3faf
Oliver Shi <oshi@yext.com>
2023-05-26 12:16:26 -0400

    add max diff pixel ratio

--- e2e-tests/tests/nested-props.spec.ts
+++ e2e-tests/tests/nested-props.spec.ts
@@ -4,5 +4,7 @@ import { studioTest } from "./infra/studioTest.js";
 studioTest("renders nested props", async ({ page, studioPage }) => {
   await studioPage.setActiveComponent("Banner");
   await studioPage.clickPropertiesTab();
-  await expect(page.getByTestId("EditorSidebar")).toHaveScreenshot();
+  await expect(page.getByTestId("EditorSidebar")).toHaveScreenshot({
+    maxDiffPixelRatio: 0.015
+  });
 });
