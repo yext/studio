7ffc1e3612ecf798063c07a0d0e4d33972115aaf
Oliver Shi <oshi@yext.com>
2023-05-31 15:12:42 -0400

    change StudioData to have a StudioConfigWithDefaulting

--- packages/studio-plugin/src/types/StudioData.ts
+++ packages/studio-plugin/src/types/StudioData.ts
@@ -1,12 +1,12 @@
 import { FileMetadata } from "./FileMetadata";
 import { SiteSettings } from "./SiteSettings";
 import { ErrorPageState, PageState } from "./PageState";
-import { StudioConfig } from "./StudioConfig";
+import { StudioConfigWithDefaulting } from "./StudioConfig";
 
 export interface StudioData {
   pageNameToPageState: Record<string, PageState>;
   pageNameToErrorPageState: Record<string, ErrorPageState>;
   UUIDToFileMetadata: Record<string, FileMetadata>;
   siteSettings?: SiteSettings;
-  studioConfig: StudioConfig;
+  studioConfig: StudioConfigWithDefaulting;
 }
