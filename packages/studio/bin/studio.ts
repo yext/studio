8dc63fb2c62386597363ecf5f88e9dc202b289cc
Oliver <oshi@yext.com>
2023-06-05 13:12:31 -0700

    experimental bin changes

--- packages/studio/bin/studio.ts
+++ packages/studio/bin/studio.ts
@@ -2,12 +2,18 @@
 import { execSync } from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
+import process from "process"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+// const pathToConfig = 'file:///' + path.resolve(__dirname, "../../vite.config.ts");
+const absPathToConfig = path.resolve(__dirname, "../../vite.config.ts");
+const relativePathToConfig = 'file://' + path.relative(process.cwd(), absPathToConfig);
+// const pathToConfig = "../../vite.config.ts";
+console.log({ __dirname, absPathToConfig, relativePathToConfig, cwd: process.cwd() })
 
 execSync(
   "node --experimental-specifier-resolution=node node_modules/vite/bin/vite.js --config " +
-    path.resolve(__dirname, "../../vite.config.ts"),
+  relativePathToConfig,
   { stdio: "inherit" }
 );
