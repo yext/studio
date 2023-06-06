aa3ce897c38f69018017da832a907940504b42c2
Annabel Li <annli@yext.com>
2023-06-06 16:36:58 -0400

    Yext Studio Commit

---
+++ apps/test-site/src/pages/a9.tsx
@@ -0,0 +1,7 @@
+import { GetPath, TemplateProps } from "@yext/pages";
+
+export const getPath: GetPath<TemplateProps> = () => {
+  return "a9";
+};
+
+export default function a9() {}
---
+++ apps/test-site/src/pages/pag.tsx
@@ -0,0 +1,7 @@
+import { GetPath, TemplateProps } from "@yext/pages";
+
+export const getPath: GetPath<TemplateProps> = () => {
+  return "pag";
+};
+
+export default function pag() {}
