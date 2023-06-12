cb2f26dde325408ef5fdd7f6223087e027075cbd
Oliver <oshi@yext.com>
2023-06-12 08:27:05 -0700

    npx studio command working!

--- packages/studio/bin/studio.ts
+++ packages/studio/bin/studio.ts
@@ -2,18 +2,9 @@
 import { execSync } from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
-import process from "process"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// const pathToConfig = 'file:///' + path.resolve(__dirname, "../../vite.config.ts");
-const absPathToConfig = path.resolve(__dirname, "../../vite.config.ts");
-const relativePathToConfig = 'file://' + path.relative(process.cwd(), absPathToConfig);
-// const pathToConfig = "../../vite.config.ts";
-console.log({ __dirname, absPathToConfig, relativePathToConfig, cwd: process.cwd() })
+const pathToViteConfig = path.resolve(__dirname, "../../vite.config.ts");
 
-execSync(
-  "node --experimental-specifier-resolution=node node_modules/vite/bin/vite.js --config " +
-  relativePathToConfig,
-  { stdio: "inherit" }
-);
+execSync('node ./node_modules/vite/bin/vite.js --config ' + pathToViteConfig, { stdio: "inherit" })
