8b6ef6e64e3895ba3e6ff9ed760d40c09cb89286
Oliver Shi <oshi@yext.com>
2023-06-13 11:25:24 -0400

    add dynamicImportFromBrowser

---
+++ packages/studio/src/utils/dynamicImportFromBrowser.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/**
+ * In order to support dynamic imports from the browser on Windows,
+ * we need to add an `@fs` prefix to all imports.
+ *
+ * Otherwise, a `Not allowed to load local resource` error will be logged.
+ */
+export default function dynamicImportFromBrowser(
+  absFilepath: string
+): Promise<any> {
+  return import(/* @vite-ignore */ "/@fs/" + absFilepath);
+}
