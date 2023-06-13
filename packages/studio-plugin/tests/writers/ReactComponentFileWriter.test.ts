698ee8f979eba1aff66ac7281478a0c93e09d6ae
Annabel Li <annli@yext.com>
2023-06-13 12:12:25 -0400

    added new function to consolidate tests

--- packages/studio-plugin/tests/writers/ReactComponentFileWriter.test.ts
+++ packages/studio-plugin/tests/writers/ReactComponentFileWriter.test.ts
@@ -54,6 +54,19 @@ function createReactComponentFileWriter(
   );
 }
 
+function testComponentNameSanitation (inputName: string, outputName: string, proj: Project) {
+  const filepath = getPagePath("updatePageFile/PageWithNoDefaultFunction");
+  createReactComponentFileWriter(
+    proj,
+    filepath,
+    inputName
+  ).updateFile({componentTree: []});
+  expect(fs.writeFileSync).toHaveBeenCalledWith(
+    expect.stringContaining("PageWithNoDefaultFunction"),
+    expect.stringContaining(`export default function ${outputName}() {}`)
+  );
+}
+
 jest.mock("uuid");
 
 describe("updateFile", () => {
@@ -532,46 +545,19 @@ describe("updateFile", () => {
     it("removes all special characters in name", () => {
       const inputName = "~'!@#%^&*()+={}[]|\\/:;\"`<>,.?- \t\r\n\f";
       const outputName = "PageDefaultFromInvalidInput";
-      const filepath = getPagePath("updatePageFile/PageWithNoDefaultFunction");
-      createReactComponentFileWriter(
-        tsMorphProject,
-        filepath,
-        inputName
-      ).updateFile({componentTree: []});
-      expect(fs.writeFileSync).toHaveBeenCalledWith(
-        expect.stringContaining("PageWithNoDefaultFunction"),
-        expect.stringContaining("export default function " + outputName + "() {}")
-      );
+      testComponentNameSanitation(inputName, outputName, tsMorphProject);
     });
 
     it("removes all leading digits in name and uppercases first letter", () => {
       const inputName = "0123456789te9st";
       const outputName = "Te9St";
-      const filepath = getPagePath("updatePageFile/PageWithNoDefaultFunction");
-      createReactComponentFileWriter(
-        tsMorphProject,
-        filepath,
-        inputName
-      ).updateFile({componentTree: []});
-      expect(fs.writeFileSync).toHaveBeenCalledWith(
-        expect.stringContaining("PageWithNoDefaultFunction"),
-        expect.stringContaining("export default function " + outputName + "() {}")
-      );
+      testComponentNameSanitation(inputName, outputName, tsMorphProject);
     });
 
     it("removes all leading non-letter unicode chars and uppercases first letter", () => {
       const inputName = "àÀصʱapple";
       const outputName = "Apple";
-      const filepath = getPagePath("updatePageFile/PageWithNoDefaultFunction");
-      createReactComponentFileWriter(
-        tsMorphProject,
-        filepath,
-        inputName
-      ).updateFile({componentTree: []});
-      expect(fs.writeFileSync).toHaveBeenCalledWith(
-        expect.stringContaining("PageWithNoDefaultFunction"),
-        expect.stringContaining("export default function " + outputName + "() {}")
-      );
+      testComponentNameSanitation(inputName, outputName, tsMorphProject);
     });
     
   });
