50f241b6a9a4e85e45fc88ebb70a2cea62f18e65
Oliver Shi <oshi@yext.com>
2023-06-16 10:18:49 -0400

    autofix

--- packages/studio/bin/studio.ts
+++ packages/studio/bin/studio.ts
@@ -6,8 +6,8 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const pathToViteConfig = path.resolve(__dirname, "../../vite.config.ts");
-const NODE_OPTIONS = "NODE_OPTIONS=\"--experimental-specifier-resolution=node\""
+const NODE_OPTIONS = 'NODE_OPTIONS="--experimental-specifier-resolution=node"';
 
 execSync(`cross-env ${NODE_OPTIONS} npx vite --config ${pathToViteConfig}`, {
-  stdio: "inherit"
+  stdio: "inherit",
 });
