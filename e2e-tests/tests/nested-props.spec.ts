be716562ce7dabb9d2ceead8c64484f82407fcc4
nmanu1 <nmanu@yext.com>
2023-06-20 18:10:29 -0400

    Update nested props screenshot

--- e2e-tests/tests/nested-props.spec.ts
+++ e2e-tests/tests/nested-props.spec.ts
@@ -2,6 +2,7 @@ import { expect } from "@playwright/test";
 import { studioTest } from "./infra/studioTest.js";
 
 studioTest("renders nested props", async ({ page, studioPage }) => {
+  await studioPage.addElement("Banner", "Components");
   await studioPage.setActiveComponent("Banner");
   await expect(page.getByTestId("EditorSidebar")).toHaveScreenshot({
     maxDiffPixelRatio: 0.015,
