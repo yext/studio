fd66a3478980bbf1bf66348612527412abc863b4
Tom Meyer <tmeyer@yext.com>
2023-06-28 14:02:48 -0400

    Revert "Update screenshots."
    This reverts commit 0857e876e0080a5a3963478ef7f2ce56a7f480ea.

--- e2e-tests/tests/infra/StudioPlaywrightPage.ts
+++ e2e-tests/tests/infra/StudioPlaywrightPage.ts
@@ -96,6 +96,7 @@ export default class StudioPlaywrightPage {
         name: `Add ${elementName} Element`,
       })
       .click();
+    await this.addElementButton.click();
   }
 
   async removeElement(elementName: string, index?: number) {
