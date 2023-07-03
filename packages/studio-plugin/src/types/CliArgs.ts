e0ad977746fee7ac59fae370d9adaf9620eb14d5
Oliver Shi <oshi@yext.com>
2023-07-03 16:08:09 -0400

    add root cli arg

---
+++ packages/studio-plugin/src/types/CliArgs.ts
@@ -0,0 +1,8 @@
+export interface CliArgs {
+  // Path to the user's root directory for studio
+  root?: string;
+  // Any arguments present after double dashes when invoking studio, e.g.
+  // `npx studio -- args like these` will result in ['args', 'like', 'these']
+  // Not currently used for anything but always provided by the cac package
+  "--": string[];
+}
--- packages/studio/bin/studio.ts
+++ packages/studio/bin/studio.ts
@@ -2,15 +2,31 @@
 import { execSync } from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
+import cac from "cac";
+import { CliArgs } from "@yext/studio-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const pathToViteConfig = path.resolve(__dirname, "../../vite.config.ts");
 const NODE_OPTIONS = 'NODE_OPTIONS="--experimental-specifier-resolution=node"';
 
-execSync(
-  `npx cross-env ${NODE_OPTIONS} npx vite --config "${pathToViteConfig}"`,
-  {
-    stdio: "inherit",
-  }
-);
+const cli = cac();
+
+cli
+  .command("", "start dev server")
+  .option("--root <directory>", `[string] path to the root directory`)
+  .action((options: CliArgs) => {
+    execSync(
+      `npx cross-env ${NODE_OPTIONS} npx vite --config "${pathToViteConfig}"`,
+      {
+        stdio: "inherit",
+        env: {
+          ...process.env,
+          YEXT_STUDIO_ARGS: JSON.stringify(options),
+        },
+      }
+    );
+  });
+
+cli.help();
+cli.parse();
