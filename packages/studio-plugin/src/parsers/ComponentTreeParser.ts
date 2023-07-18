1014f7a58a185641557f7f01d790dc8804ce78c5
Alex Taing <ataing@yext.com>
2023-07-18 16:45:44 -0400

    reduce nesting

--- packages/studio-plugin/src/parsers/ComponentTreeParser.ts
+++ packages/studio-plugin/src/parsers/ComponentTreeParser.ts
@@ -227,46 +227,48 @@ function getMissingRequiredProps(
   propShape: PropShape | undefined
 ): string[] {
   const missingProps: string[] = [];
-  if (propShape) {
-    for (const propName of Object.keys(propShape)) {
-      const propIsRequired = propShape[propName].required;
-      const propIsUndefined = propValues[propName] === undefined;
-      if (propIsRequired && propIsUndefined) {
-        missingProps.push(propName);
-      }
-      if (propIsUndefined) {
+  if (!propShape) {
+    return [];
+  }
+  
+  for (const propName of Object.keys(propShape)) {
+    const propIsRequired = propShape[propName].required;
+    const propIsUndefined = propValues[propName] === undefined;
+    if (propIsRequired && propIsUndefined) {
+      missingProps.push(propName);
+    }
+    if (propIsUndefined) {
+      continue;
+    }
+
+    const shapeMetadata = propShape[propName];
+    const valueMetadata = propValues[propName];
+    if (shapeMetadata.type === PropValueType.Array) {
+      if (
+        shapeMetadata.itemType.type !== PropValueType.Object ||
+        valueMetadata.valueType !== PropValueType.Array ||
+        valueMetadata.kind !== PropValueKind.Literal
+      ) {
         continue;
       }
-
-      const shapeMetadata = propShape[propName];
-      const valueMetadata = propValues[propName];
-      if (shapeMetadata.type === PropValueType.Array) {
-        if (
-          shapeMetadata.itemType.type !== PropValueType.Object ||
-          valueMetadata.valueType !== PropValueType.Array ||
-          valueMetadata.kind !== PropValueKind.Literal
-        ) {
-          continue;
-        }
-        const arrayShapes = shapeMetadata.itemType.shape;
-        for (const value of valueMetadata.value) {
-          if (value.valueType === PropValueType.Object) {
-            const missingArrayProps = getMissingRequiredProps(
-              value.value,
-              arrayShapes
-            );
-            missingProps.push(...missingArrayProps);
-          }
-        }
-      } else if (shapeMetadata.type === PropValueType.Object) {
-        if (valueMetadata.valueType === PropValueType.Object) {
-          const objectShape = shapeMetadata.shape;
-          const missingObjectProps = getMissingRequiredProps(
-            valueMetadata.value,
-            objectShape
+      const arrayShapes = shapeMetadata.itemType.shape;
+      for (const value of valueMetadata.value) {
+        if (value.valueType === PropValueType.Object) {
+          const missingArrayProps = getMissingRequiredProps(
+            value.value,
+            arrayShapes
           );
-          missingProps.push(...missingObjectProps);
+          missingProps.push(...missingArrayProps);
         }
+      }
+    } else if (shapeMetadata.type === PropValueType.Object) {
+      if (valueMetadata.valueType === PropValueType.Object) {
+        const objectShape = shapeMetadata.shape;
+        const missingObjectProps = getMissingRequiredProps(
+          valueMetadata.value,
+          objectShape
+        );
+        missingProps.push(...missingObjectProps);
       }
     }
   }
