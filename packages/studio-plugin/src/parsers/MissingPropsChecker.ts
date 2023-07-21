391fd9f4f8edf6186041a1825891359c0e509bb5
Alex Taing <ataing@yext.com>
2023-07-21 15:19:51 -0400

    handle arrays nested in arrays

--- packages/studio-plugin/src/parsers/MissingPropsChecker.ts
+++ packages/studio-plugin/src/parsers/MissingPropsChecker.ts
@@ -1,5 +1,7 @@
 import {
 	PropShape,
+	PropType,
+	PropVal,
 	PropValueKind,
 	PropValueType,
 	PropValues
@@ -31,21 +33,28 @@ export default class MissingPropsChecker {
 			const valueMetadata = propValues[propName];
 			if (shapeMetadata.type === PropValueType.Array) {
 				if (
-					shapeMetadata.itemType.type !== PropValueType.Object ||
-					valueMetadata.valueType !== PropValueType.Array ||
-					valueMetadata.kind !== PropValueKind.Literal
+					shapeMetadata.itemType.type === PropValueType.Object &&
+					valueMetadata.valueType === PropValueType.Array &&
+					valueMetadata.kind === PropValueKind.Literal
 				) {
-					continue;
-				}
-				const arrayShapes = shapeMetadata.itemType.shape;
-				for (const value of valueMetadata.value) {
-					if (value.valueType === PropValueType.Object) {
-						const missingArrayProps = this.getMissingRequiredProps(
-							value.value,
-							arrayShapes
-						);
-						missingProps.push(...missingArrayProps);
+					const arrayShapes = shapeMetadata.itemType.shape;
+					for (const value of valueMetadata.value) {
+						if (value.valueType === PropValueType.Object) {
+							const missingArrayProps = this.getMissingRequiredProps(
+								value.value,
+								arrayShapes
+							);
+							missingProps.push(...missingArrayProps);
+						}
 					}
+				} else if (
+					shapeMetadata.itemType.type === PropValueType.Array &&
+					valueMetadata.valueType === PropValueType.Array &&
+					valueMetadata.kind === PropValueKind.Literal
+				) {
+					const arrayShapes = shapeMetadata.itemType;
+					const missingArrayProps = this.handleNestedArrays(valueMetadata.value, arrayShapes)
+					missingProps.push(...missingArrayProps);
 				}
 			} else if (shapeMetadata.type === PropValueType.Object) {
 				if (valueMetadata.valueType === PropValueType.Object) {
@@ -60,4 +69,29 @@ export default class MissingPropsChecker {
 		}
 		return missingProps;
 	}   
+
+	handleNestedArrays(
+		valueArray: PropVal[], 
+		shape: PropType
+	): string[] {
+		const missingProps: string[] = [];
+		for (const value of valueArray) {
+			if (
+				value.valueType === PropValueType.Object &&
+				shape.type == PropValueType.Object
+			) {
+				const missingObjectProps = this.getMissingRequiredProps(value.value, shape.shape)
+				missingProps.push(...missingObjectProps);
+			} else if (
+				value.kind === PropValueKind.Literal &&
+				value.valueType === PropValueType.Array &&
+				shape.type === PropValueType.Array
+			) {
+				const missingObjectProps = this.handleNestedArrays(value.value, shape.itemType);
+				missingProps.push(...missingObjectProps);
+			}
+		}
+		return missingProps
+	}
 }
+
