ad05b6e55b70cd532f960247526f03768e5bf100
Annabel Li <annli@yext.com>
2023-07-21 18:08:20 -0400

    add function back

--- packages/studio-plugin/src/utils/ComponentTreeHelpers.ts
+++ packages/studio-plugin/src/utils/ComponentTreeHelpers.ts
@@ -112,16 +112,21 @@ export default class ComponentTreeHelpers {
     const expressions: string[] = this.getExpressions(componentTree);
     if (getPathValue)
       expressions.push(...this.getExpressionUsagesFromPropVal(getPathValue));
-    return expressions.flatMap(e => {
-      if (TypeGuards.isTemplateString(e)) {
-        return [...e.matchAll(TEMPLATE_STRING_EXPRESSION_REGEX)]
+    return expressions.flatMap(e => filterExpressionWithSource(e, source));
+
+    const filterExpressionWithSource = (
+      expression: string,
+      source: string
+    ) => {
+      if (TypeGuards.isTemplateString(expression)) {
+        return [...expression.matchAll(TEMPLATE_STRING_EXPRESSION_REGEX)]
           .map((m) => m[1])
           .filter((m) => ExpressionHelpers.usesExpressionSource(m, source));
       }
-      if (ExpressionHelpers.usesExpressionSource(e, source))
-        return [e];
+      if (ExpressionHelpers.usesExpressionSource(expression, source))
+        return [expression];
       return [];
-    });
+    }
   }
 
   /**
