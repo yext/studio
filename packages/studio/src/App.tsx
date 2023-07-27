9df4b952ddc5875766175e2a477b0c150fd406f0
Annabel Li <annli@yext.com>
2023-07-27 10:05:15 -0400

    remove a new line

--- packages/studio/src/App.tsx
+++ packages/studio/src/App.tsx
@@ -5,7 +5,6 @@ import PreviewWithUseComponents from "./components/PreviewWithUseComponents";
 import LeftSidebar from "./components/LeftSidebar";
 
 export default function App() {
-  
   return (
     <div className="App">
       <Toast />
