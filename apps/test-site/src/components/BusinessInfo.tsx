e9ccf72561e080b088da6fb09f086fed38de4f86
Tom Meyer <tmeyer@yext.com>
2023-08-02 15:19:59 -0400

    Incremental improvements to the Test Site.
    This PR beings making improvements to the Test Site. Our goal is
    to have the Test Site look and feel like a real customer site. To
    that end, I created a new `LocationPage` template. This will
    eventually take the place of `UniversalPage`. In addition to the
    new Entity Template, I added Components for displaying Addresses,
    Hours, a Header, and a Featured Image. I also added a Component,
    called `BusinessContainer` that allows you to group Hours and
    Address information side by side.
    
    I will contine making incremental improvements after this PR. My
    goal is to have our Playwright Tests operating on a more realistic
    looking Site.
    
    TEST=manual
    
    Made sure the new Components could be added to the new Page Template.
    Ensured rough styling looked good.

---
+++ apps/test-site/src/components/BusinessInfo.tsx
@@ -0,0 +1,15 @@
+export interface BusinessInfoProps {
+  title?: string,
+  children: React.ReactNode
+}
+
+export default function BusinessInfo({ title, children}: BusinessInfoProps) {
+  return (
+    <div className="flex flex-col bg-slate-100">
+      {title && <h2 className="text-3xl">{title}</h2>}
+      <div className="flex flex-row justify-around">
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ apps/test-site/src/templates/LocationPage.tsx
@@ -0,0 +1,61 @@
+import { GetPath, TemplateConfig, TemplateProps } from "@yext/pages";
+import Address from "../components/Address";
+import BusinessInfo from "../components/BusinessInfo";
+import Header from "../components/Header";
+import HoursDisplay from "../components/HoursDisplay";
+import ProminentImage from "../components/ProminentImage";
+
+export const config: TemplateConfig = {
+  stream: {
+    $id: "studio-stream-id",
+    localization: { locales: ["en"], primary: false },
+    filter: { entityTypes: ["location"] },
+    fields: ["slug"],
+  },
+};
+export const getPath: GetPath<TemplateProps> = ({ document }) => {
+  return `${document.slug}`;
+};
+
+export default function LocationPage() {
+  return (
+    <>
+      <Header
+        title="Yext"
+        logo="https://a.mktgcdn.com/p/R9FjcYjRNA5dAespqgHFLMvu2m18-E5Apnb3KON0oJY/300x300.png"
+        backgroundColor="#BAD8FD"
+      />
+      <ProminentImage src="https://images.ctfassets.net/n2ifzifcqscw/10wJSHT2Zvj5G1Z3GYHUqv/882e93cefece92d25d25933d56598903/telluride_shutterstock_2074692298.jpg" />
+      <BusinessInfo>
+        <Address
+          line1={`110 Founders Ave`}
+          city={`Falls Church`}
+          region={`VA`}
+          postalCode={`22046`}
+          countryCode=""
+        />
+        <HoursDisplay
+          monday={{
+            isClosed: false,
+            openIntervals: [
+              { start: `9:00 AM`, end: `11:00 AM` },
+              { start: `12:00 PM`, end: `5:00 PM` },
+            ],
+          }}
+          tuesday={{
+            isClosed: false,
+            openIntervals: [{ start: `9:00 AM`, end: `5:00 PM` }],
+          }}
+          thursday={{
+            isClosed: false,
+            openIntervals: [
+              { start: `9:00 AM`, end: `11:00 AM` },
+              { start: `12:00 PM`, end: `2:00 PM` },
+              { start: `4:00 PM`, end: `5:00 PM` },
+            ],
+          }}
+        />
+      </BusinessInfo>
+    </>
+  );
+}
