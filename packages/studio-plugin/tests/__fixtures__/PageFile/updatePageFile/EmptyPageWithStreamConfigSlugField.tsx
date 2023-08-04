043cb472da265b8d98db0312c23320190883b213
nmanu1 <nmanu@yext.com>
2023-08-04 08:53:10 -0400

    Use unique stream ids

--- packages/studio-plugin/tests/__fixtures__/PageFile/updatePageFile/EmptyPageWithStreamConfigSlugField.tsx
+++ packages/studio-plugin/tests/__fixtures__/PageFile/updatePageFile/EmptyPageWithStreamConfigSlugField.tsx
@@ -2,7 +2,7 @@ import { TemplateConfig, TemplateProps } from "@yext/pages";
 
 export const config: TemplateConfig = {
   stream: {
-    $id: "studio-stream-id",
+    $id: "studio-stream-id-test",
     localization: { locales: ["en"], primary: false },
     filter: {},
     fields: ["slug"],
