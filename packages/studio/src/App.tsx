6dc6bb1d49996ae3393cd8a0b9e115b7464c4934
Annabel Li <annli@yext.com>
2023-08-08 12:15:51 -0400

    widget setup

--- packages/studio/src/App.tsx
+++ packages/studio/src/App.tsx
@@ -3,8 +3,22 @@ import ActionsBar from "./components/ActionsBar";
 import Toast from "./components/Toast";
 import PreviewWithUseComponents from "./components/PreviewWithUseComponents";
 import LeftSidebar from "./components/LeftSidebar";
+import { Widget, addResponseMessage } from 'react-chat-widget';
+import { useCallback, useEffect } from "react";
+
+import 'react-chat-widget/lib/styles.css';
 
 export default function App() {
+  useEffect(() => {
+    addResponseMessage('Welcome to this **awesome** chat!');
+  }, []);
+
+  const handleNewUserMessage = useCallback((newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+    addResponseMessage("hello");
+  }, []);
+
   return (
     <div className="App">
       <Toast />
@@ -18,6 +32,9 @@ export default function App() {
           <EditorSidebar />
         </div>
       </div>
+      <Widget 
+        handleNewUserMessage={handleNewUserMessage} 
+      />
     </div>
   );
 }
