0bab5a02951a8e59f1a2d2a0343fae56632cab5e
Alex Taing <ataing@yext.com>
2023-08-10 16:29:04 -0400

    new approach modify workspace

--- generate-third-party-notices.sh
+++ generate-third-party-notices.sh
@@ -1,9 +1,12 @@
-PACKAGE_DIR=$(pwd)
-REPO_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-echo "copying package $PACKAGE_DIR to $REPO_DIR/temp"
-rm -rf "$REPO_DIR/temp"
-cp -r $PACKAGE_DIR "$REPO_DIR/temp"
-cd "$REPO_DIR/temp" && npm i
-npx generate-license-file --input package.json --output THIRD-PARTY-NOTICES --overwrite
-cp "$REPO_DIR/temp/THIRD-PARTY-NOTICES" $PACKAGE_DIR
-rm -rf "$REPO_DIR/temp"
+WORKSPACES=$(json -f package.json workspaces)
+json -I -f package.json -e 'this.workspaces=[]'
+cd packages/studio
+npm install
+generate-license-file --input package.json --output THIRD-PARTY-NOTICES --overwrite
+rm package-lock.json
+cd ../studio-plugin
+npm install
+generate-license-file --input package.json --output THIRD-PARTY-NOTICES --overwrite
+rm package-lock.json
+cd ../..
+json -I -f package.json -e "this.workspaces=${WORK}"
