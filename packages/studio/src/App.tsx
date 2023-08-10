3fa3848825b9616f70e606de236753a870718ce5
Alex Taing <ataing@yext.com>
2023-08-10 10:52:36 -0400

    change to npm install

--- packages/studio/src/App.tsx
+++ packages/studio/src/App.tsx
@@ -4,20 +4,20 @@ import Toast from "./components/Toast";
 import PreviewWithUseComponents from "./components/PreviewWithUseComponents";
 import LeftSidebar from "./components/LeftSidebar";
 
-  export default function App() {
-    return (
-      <div className="App">
-        <Toast />
-        <div className="flex flex-col w-screen h-screen">
-          <ActionsBar />
-          <div className="flex flex-row grow">
-            <LeftSidebar />
-            <div className="grow w-1/3 bg-white border-8 shadow">
-              <PreviewWithUseComponents />
-            </div>
-            <EditorSidebar />
+export default function App() {
+  return (
+    <div className="App">
+      <Toast />
+      <div className="flex flex-col w-screen h-screen">
+        <ActionsBar />
+        <div className="flex flex-row grow">
+          <LeftSidebar />
+          <div className="grow w-1/3 bg-white border-8 shadow">
+            <PreviewWithUseComponents />
           </div>
+          <EditorSidebar />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
