64d4e6165f5b5fba031531dbd9179466b779b5a1
Alex Taing <ataing@yext.com>
2023-08-10 12:02:46 -0400

    yen's script

---
+++ generate-third-party-notices.sh
@@ -0,0 +1,9 @@
+PACKAGE_DIR=$(pwd)
+REPO_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+echo "copying package $PACKAGE_DIR to $REPO_DIR/temp"
+rm -rf "$REPO_DIR/temp"
+cp -r $PACKAGE_DIR "$REPO_DIR/temp"
+cd "$REPO_DIR/temp" && npm i
+generate-license-file --input package.json --output THIRD-PARTY-NOTICES --overwrite
+cp "$REPO_DIR/temp/THIRD-PARTY-NOTICES" $PACKAGE_DIR
+rm -rf "$REPO_DIR/temp"
