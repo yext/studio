4733bf3753ef44fc654febd6fb08e5df73069e03
Brian Stephan <bstephan@yext.com>
2023-08-13 22:36:41 -0400

    Remove inconsistent dropdown scrolling tests

--- e2e-tests/tests/field-picker-dropdown.spec.ts
+++ e2e-tests/tests/field-picker-dropdown.spec.ts
@@ -7,7 +7,4 @@ studioTest("renders field picker dropdown", async ({ page, studioPage }) => {
   await page.getByLabel("Toggle field picker").click();
   await page.getByText("Meta").click();
   await expect(page).toHaveScreenshot();
-  await page.setViewportSize({ width: 1280, height: 250 });
-  await page.mouse.wheel(0, 150);
-  await expect(page).toHaveScreenshot();
 });
