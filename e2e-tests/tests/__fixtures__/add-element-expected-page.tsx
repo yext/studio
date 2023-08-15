07401342752fc7b28120e39b8d8da5de827e97ea
Tom Meyer <tmeyer@yext.com>
2023-08-15 08:00:00 -0700

    Add Location Entity Template to E2E Test Site.  (#331)
    This PR is the first step in making our `e2e-tests` a bit more realistic. We will have Templates and Components that are a bit more complex. Our end goal is for the `e2e-tests` repo to look and feel like a (somewhat) real PagesJS repo. This ensures that we are running our acceptance and visual regression tests in scenarios that are as real life as possible. Eventually, we will want to have a full end-to-end test that makes a change in Studio and sees it propagate through the
    PagesJS `dev` and `build` commands.
    
    For now, we add a more realistic Template for a Location Page, as well as some common Components for such a page. The `add-element` test was updated to add a Footer to the Location Template.
    
    TEST=auto
    
    Ran all unit tests and re-generated all the necessary snapshots. After re-generating the snapshots, I did spot check a good number of them. I checked all the snapshots for the modified `add-element` test.

--- e2e-tests/tests/__fixtures__/add-element-expected-page.tsx
+++ e2e-tests/tests/__fixtures__/add-element-expected-page.tsx
@@ -1,20 +1,56 @@
-import { GetPath, TemplateProps } from "@yext/pages";
-import Banner from "../components/Banner";
-import Container from "../components/Container";
+import { GetPath, TemplateConfig, TemplateProps } from "@yext/pages";
+import AddressDisplay from "../components/AddressDisplay";
+import BusinessInfo from "../components/BusinessInfo";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import HoursDisplay from "../components/HoursDisplay";
 
-export const getPath: GetPath<TemplateProps> = () => {
-  return "index.html";
+export const config: TemplateConfig = {
+  stream: {
+    $id: "studio-stream-id-LocationPage",
+    localization: { locales: ["en"], primary: false },
+    filter: { entityTypes: ["location"] },
+    fields: ["address", "hours", "slug"],
+  },
+};
+export const getPath: GetPath<TemplateProps> = ({
+  document,
+}: TemplateProps) => {
+  return `${document.slug}`;
 };
 
-export default function BasicPage() {
+export default function LocationPage({ document }: TemplateProps) {
   return (
     <>
-      <Container />
-      <div>
-        <Container>
-          <Banner bool={false} num={0} />
-        </Container>
-      </div>
+      <Footer copyrightText="© 2023 Yext" backgroundColor="#BAD8FD" />
+      <Header
+        title="Yext"
+        logo="https://a.mktgcdn.com/p/R9FjcYjRNA5dAespqgHFLMvu2m18-E5Apnb3KON0oJY/300x300.png"
+        backgroundColor="#BAD8FD"
+      />
+      <BusinessInfo>
+        <AddressDisplay
+          line1={`${document.address.line1}`}
+          city={`${document.address.city}`}
+          region={`${document.address.region}`}
+          postalCode={`${document.address.postalCode}`}
+          countryCode=""
+        />
+        <HoursDisplay
+          monday={{
+            isClosed: false,
+            openIntervals: document.hours.monday.openIntervals,
+          }}
+          tuesday={{
+            isClosed: false,
+            openIntervals: document.hours.tuesday.openIntervals,
+          }}
+          wednesday={{
+            isClosed: false,
+            openIntervals: document.hours.wednesday.openIntervals,
+          }}
+        />
+      </BusinessInfo>
     </>
   );
 }
