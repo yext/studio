2dcc114a06bcee7ffef9105408452bf7623ec8d2
Oliver Shi <oshi@yext.com>
2023-08-22 13:14:58 -0400

    another wip

--- packages/studio/src/components/TailwindPropInput.tsx
+++ packages/studio/src/components/TailwindPropInput.tsx
@@ -3,6 +3,7 @@ import generateTailwindSafelist from '../utils/generateTailwindSafelist'
 import { StudioTailwindTheme } from '@yext/studio-plugin';
 import { ReactComponent as EmbedIcon } from "../icons/embed.svg";
 import { useRootClose } from '@restart/ui';
+import classNames from "classnames";
 
 const safelistPromise: Promise<string[] | undefined> = import('@pathToUserProjectRoot/tailwind.config').then(module => {
   const theme: StudioTailwindTheme | undefined = module.default?.theme?.extend;
@@ -14,10 +15,6 @@ interface Props {
   value: string,
   disabled: boolean
 }
-
-
-// const handleSelect = useCallback(() => {
-// }, [])
 
 
 export default function TailwindPropInput({ onChange, value, disabled }: Props) {
@@ -35,30 +32,44 @@ export default function TailwindPropInput({ onChange, value, disabled }: Props) 
     setIsOpen(false)
   })
 
+  const toggleOpen = useCallback(() => setIsOpen(!isOpen), [isOpen])
+  const addTailwindClass = useCallback((tailwindClass: string) => {
+    setIsOpen(false);
+    const tailwindClasses = new Set(value.trim().split(" "))
+    tailwindClasses.add(tailwindClass)
+    const combinedClass = [...tailwindClasses].join(" ");
+    onChange(combinedClass);
+  }, [onChange, value])
+
+  const hasUnusedClasses = !!safelist?.length
+  const valueClassname = classNames("border border-gray-300 focus:border-indigo-500 rounded-lg pt-2 pb-1 pl-2 w-full", {
+    "pr-8": hasUnusedClasses
+  })
+
   return (
     <div className='relative' ref={ref}>
-      <div
-        className="border border-gray-300 focus:border-indigo-500 rounded-lg pt-2 pb-1 pl-2 w-full pr-8"
-      >
+      <div className={valueClassname}>
         {value.trim().split(' ').map(tailwindClass => {
           return (
-            <span className='mr-1 mb-1 inline-block bg-sky-100 rounded px-1'>{tailwindClass}</span>
+            <span className='mr-1 mb-1 inline-block bg-sky-100 rounded px-1 hover:bg-sky-200'>{tailwindClass}</span>
           )
         })}
       </div>
-      <i className="absolute right-0 top-2.5 mr-2 bg-white not-italic">
-        <EmbedIcon
-          role="button"
-          aria-label="Toggle field picker"
-          onClick={() => setIsOpen(!isOpen)}
-        />
-      </i>
-      {isOpen && safelist && renderDropdown(safelist, value, onChange)}
+      {hasUnusedClasses && 
+        <i className="absolute right-0 top-2.5 mr-2 bg-white not-italic">
+          <EmbedIcon
+            role="button"
+            aria-label="Toggle field picker"
+            onClick={toggleOpen}
+          />
+        </i>
+      }
+      {isOpen && hasUnusedClasses && renderDropdown(safelist, value, addTailwindClass)}
     </div>
   )
 }
 
-function renderDropdown(safelist: string[], value: string, onChange: (val: string) => void) {
+function renderDropdown(safelist: string[], value: string, addTailwindClass: (val: string) => void) {
   return (
     <ul
       className="absolute w-max bg-white mt-2 rounded border shadow-2xl z-10"
@@ -73,10 +84,6 @@ function renderDropdown(safelist: string[], value: string, onChange: (val: strin
             className="hover:bg-gray-100 px-4 py-1 cursor-pointer flex justify-between"
             key={tailwindClass}
             onClick={() => {
-              const tailwindClasses = new Set(value.trim().split(" "))
-              tailwindClasses.add(tailwindClass)
-              const combinedClass = [...tailwindClasses].join(" ");
-              onChange(combinedClass)
             }}
           >
             {tailwindClass}
