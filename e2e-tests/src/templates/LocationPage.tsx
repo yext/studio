b0622abe904e09a22c6a4fcd9d1b63253e3773f5
Alex Taing <ataing@yext.com>
2023-08-28 13:20:24 -0400

    add prominent image

--- e2e-tests/src/templates/LocationPage.tsx
+++ e2e-tests/src/templates/LocationPage.tsx
@@ -3,6 +3,7 @@ import AddressDisplay from "../components/AddressDisplay";
 import BusinessInfo from "../components/BusinessInfo";
 import Header from "../components/Header";
 import HoursDisplay from "../components/HoursDisplay";
+import ProminentImage from "../components/ProminentImage";
 
 export const config: TemplateConfig = {
   stream: {
@@ -26,6 +27,7 @@ export default function LocationPage({ document }: TemplateProps) {
         logo="https://a.mktgcdn.com/p/R9FjcYjRNA5dAespqgHFLMvu2m18-E5Apnb3KON0oJY/300x300.png"
         backgroundColor="#BAD8FD"
       />
+      <ProminentImage src="https://images.ctfassets.net/n2ifzifcqscw/10wJSHT2Zvj5G1Z3GYHUqv/882e93cefece92d25d25933d56598903/telluride_shutterstock_2074692298.jpg" />
       <BusinessInfo>
         <AddressDisplay
           line1={`${document.address.line1}`}
