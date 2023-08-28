6e93441d1015bd7cb75d839f6c728057e36aa752
Oliver Shi <oshi@yext.com>
2023-08-28 14:34:28 -0400

    update

--- e2e-tests/tests/undefined-menu-button.spec.ts
+++ e2e-tests/tests/undefined-menu-button.spec.ts
@@ -5,6 +5,4 @@ studioTest("renders undefined menu button", async ({ page, studioPage }) => {
   await studioPage.setActiveComponent("Banner");
   await page.getByLabel("Toggle undefined value menu").first().click();
   await expect(page).toHaveScreenshot();
-  await page.getByText("Reset to Default").hover();
-  await page.mouse.wheel(200, 0);
 });
