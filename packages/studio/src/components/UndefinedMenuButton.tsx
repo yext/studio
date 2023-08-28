292d3be3071055f54f1531e3e5908dd5db65cfc5
Oliver Shi <oshi@yext.com>
2023-08-28 16:54:41 -0400

    tweak styling

--- packages/studio/src/components/UndefinedMenuButton.tsx
+++ packages/studio/src/components/UndefinedMenuButton.tsx
@@ -45,7 +45,7 @@ export default function UndefinedMenuButton({
         className="absolute right-0 top-1"
       />
       {isOpen && (
-        <div className="relative mt-12">
+        <div className="relative mt-5">
           <div
             className="absolute bg-white rounded border z-10 shadow-2xl text-sm right-0 cursor-pointer hover:bg-gray-100 px-2 py-1 whitespace-nowrap"
             onClick={onMenuClick}
