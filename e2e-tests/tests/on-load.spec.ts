80e67a34458bd386452792911de6df061b0ddac3
Oliver Shi <oshi@yext.com>
2023-08-29 14:54:17 -0400

    remove only

--- e2e-tests/tests/on-load.spec.ts
+++ e2e-tests/tests/on-load.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from "@playwright/test";
 import { studioTest } from "./infra/studioTest.js";
 
-studioTest.only("page appears as expected on load", async ({ page, studioPage }) => {
+studioTest("page appears as expected on load", async ({ page, studioPage }) => {
   await expect(studioPage.saveButton.button).toBeDisabled();
   await expect(page).toHaveScreenshot();
 });
