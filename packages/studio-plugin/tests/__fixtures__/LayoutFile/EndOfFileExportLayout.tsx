185df689414ddbfc8107dcedf4893d49b66b52b6
Alex Taing <ataing@yext.com>
2023-09-06 16:52:28 -0400

    add some tests

---
+++ packages/studio-plugin/tests/__fixtures__/LayoutFile/EndOfFileExportLayout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { TemplateProps } from "@yext/pages";
+
+function EndOfFileExportLayout({ document }: TemplateProps) {
+    return <p>export default is at the end of the file</p>
+}
+
+export default EndOfFileExportLayout;
---
+++ packages/studio-plugin/tests/__fixtures__/LayoutFile/FunctionInlineExportLayout.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { TemplateProps } from "@yext/pages";
+
+export default function FunctionInlineExportLayout({ document }: TemplateProps) {
+    return <p>export default is inline with the function declaration</p>
+}
