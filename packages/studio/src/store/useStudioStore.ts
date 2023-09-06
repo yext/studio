fd1da2af9d9876bd5387443793cbf8da98f23668
Nidhi Manu <nmanu@yext.com>
2023-09-06 15:37:47 -0400

    Add parentheses

--- packages/studio/src/store/useStudioStore.ts
+++ packages/studio/src/store/useStudioStore.ts
@@ -56,8 +56,10 @@ const useStudioStore = create<StudioStore>()(
 );
 
 void useStudioStore.getState().actions.refreshActivePageEntities();
-void useStudioStore.getState().studioConfig.isPagesJSRepo &&
-  useStudioStore.getState().accountContent.refreshBaseAccountContent();
+void (
+  useStudioStore.getState().studioConfig.isPagesJSRepo &&
+  useStudioStore.getState().accountContent.refreshBaseAccountContent()
+);
 
 export type UseStudioStore = typeof useStudioStore;
 export default useStudioStore;
