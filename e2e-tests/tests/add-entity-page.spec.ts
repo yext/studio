dd20ab8e266df7b5c887646dc282e9e93c6aff90
Nidhi Manu <nmanu@yext.com>
2023-09-06 15:12:22 -0400

    Update TODO

--- e2e-tests/tests/add-entity-page.spec.ts
+++ e2e-tests/tests/add-entity-page.spec.ts
@@ -11,7 +11,8 @@ studioTest("can add an entity page", async ({ page, studioPage }) => {
   const pageInTree = page.getByText("EntityPage");
   await expect(pageInTree).toHaveCount(0);
 
-  // TODO (SLAP-2906): Specify a stream scope once account content is populated
+  // TODO: Specify a stream scope once we can supply an API key for populating
+  // the store with account content
   await studioPage.addEntityPage("EntityPage", {}, "entity-page");
   await expect(pageInTree).toHaveCount(1);
   await expect(page).toHaveScreenshot();
