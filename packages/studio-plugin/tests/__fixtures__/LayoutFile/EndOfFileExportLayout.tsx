fc0cc670cde60c04b92b528b3ffa74aa001cb83a
Alex Taing <ataing@yext.com>
2023-09-07 10:00:56 -0400

    remove irrelevant tests

--- packages/studio-plugin/tests/__fixtures__/LayoutFile/EndOfFileExportLayout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TemplateProps } from "@yext/pages";
-
-function EndOfFileExportLayout({ document }: TemplateProps) {
-  return <p>export default is at the end of the file</p>;
-}
-
-export default EndOfFileExportLayout;
--- packages/studio-plugin/tests/__fixtures__/LayoutFile/FunctionInlineExportLayout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TemplateProps } from "@yext/pages";
-
-export default function FunctionInlineExportLayout({
-  document,
-}: TemplateProps) {
-  return <p>export default is inline with the function declaration</p>;
-}
