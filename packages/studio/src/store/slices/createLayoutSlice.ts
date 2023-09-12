e3029d59ed8955fdd914e9c1566338a69fabedd3
Nidhi Manu <nmanu@yext.com>
2023-09-12 17:34:08 -0400

    Add TODO

--- packages/studio/src/store/slices/createLayoutSlice.ts
+++ packages/studio/src/store/slices/createLayoutSlice.ts
@@ -3,6 +3,7 @@ import { LayoutSlice } from "../models/slices/LayoutSlice";
 import { SliceCreator } from "../models/utils";
 
 const createLayoutSlice: SliceCreator<LayoutSlice> = () => ({
+  // TODO (SLAP-2930): Remove top-level fragments from layouts
   layouts: initialStudioData.layoutNameToLayoutMetadata,
 });
 
