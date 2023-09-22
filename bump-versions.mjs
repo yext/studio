f4790879b498c4dfd65b61307a54ae580b020058
Oliver Shi <oshi@yext.com>
2023-09-22 12:16:09 -0400

    v0.23.1 (#383)
    ### Fixes
    
    * Removes the tsconfig extends from the studio tsconfig.
    * Adds virtual modules to optimizeDeps.exclude in studio's vite config.

---
+++ bump-versions.mjs
@@ -0,0 +1,30 @@
+import { execSync } from "child_process";
+import fs from "fs";
+
+const newVersion = process.argv[2];
+
+function bumpStudio() {
+  const packageJsonPath = "./packages/studio/package.json";
+  const packageJson = readJson(packageJsonPath);
+  packageJson.dependencies["@yext/studio-plugin"] = newVersion;
+  packageJson.dependencies["@yext/studio-ui"] = newVersion;
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  bumpPackage("studio");
+}
+
+function bumpPackage(packageName) {
+  console.log(`... bumping ${packageName}`);
+  execSync(`npm version ${newVersion} -w=packages/${packageName}`);
+}
+
+function readJson(filepath) {
+  return JSON.parse(fs.readFileSync(filepath, "utf-8"));
+}
+
+function main() {
+  bumpPackage("studio-plugin");
+  bumpPackage("studio-ui");
+  bumpStudio();
+}
+
+main();
