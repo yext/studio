cb7331c267363650cfe9233e9c5edb8567acd28d
Oliver Shi <oshi@yext.com>
2023-09-22 11:56:54 -0400

    update update script

--- update-minor-versions.mjs
+++ update-minor-versions.mjs
@@ -1,49 +1,30 @@
 import { execSync } from "child_process";
 import fs from "fs";
 
-function bumpStudioPlugin() {
-  console.log("... bumping studio-plugin");
-  execSync("npm version minor -w=packages/studio-plugin");
-  return readVersion("./packages/studio-plugin/package.json");
-}
+const newVersion = process.argv[2];
 
-function bumpStudio(pluginVersion, uiVersion) {
-  console.log("... bumping studio");
+function bumpStudio() {
   const packageJsonPath = "./packages/studio/package.json";
   const packageJson = readJson(packageJsonPath);
-  // `npm i @yext/studio-plugin@${newVersion} --save-exact` does not update the package json,
-  // likely because the new version does not exist yet.
-  packageJson.dependencies["@yext/studio-plugin"] = pluginVersion;
-  packageJson.dependencies["@yext/studio-ui"] = uiVersion;
+  packageJson.dependencies["@yext/studio-plugin"] = newVersion;
+  packageJson.dependencies["@yext/studio-ui"] = newVersion;
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-  execSync("npm version minor -w=packages/studio");
+  bumpPackage("studio");
 }
 
-function bumpStudioUI() {
-  console.log("... bumping studio-ui");
-  execSync("npm version minor -w=packages/studio-ui");
-  return readVersion("./packages/studio-ui/package.json");
+function bumpPackage(packageName) {
+  console.log(`... bumping ${packageName}`);
+  execSync(`npm version ${newVersion} -w=packages/${packageName}`);
 }
 
 function readJson(filepath) {
   return JSON.parse(fs.readFileSync(filepath, "utf-8"));
 }
 
-function readVersion(packageJsonPath) {
-  const packageJson = readJson(packageJsonPath);
-  const newVersion = packageJson?.version;
-  if (!newVersion) {
-    throw new Error(
-      `Could not parse version from package.json at ${packageJsonPath}`
-    );
-  }
-  return newVersion;
-}
-
 function main() {
-  const pluginVersion = bumpStudioPlugin();
-  const uiVersion = bumpStudioUI();
-  bumpStudio(pluginVersion, uiVersion);
+  bumpPackage("studio-plugin");
+  bumpPackage("studio-ui");
+  bumpStudio();
 }
 
 main();
