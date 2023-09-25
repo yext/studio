638ddfbf5d13eb5a029623192d30e961d3225574
Alex Taing <ataing@yext.com>
2023-09-25 11:21:17 -0400

    optional

--- packages/studio-ui/src/components/DeployButton.tsx
+++ packages/studio-ui/src/components/DeployButton.tsx
@@ -43,6 +43,7 @@ export default function DeployButton() {
   const isDisabled = deployInProgress || (!hasChanges && !canPush);
 
   return (
+    <div className="flex flex-col">
     <button
       className="ml-4 py-1 px-3 text-white rounded-md disabled:bg-gray-400 bg-blue-600 hover:bg-blue-700"
       onClick={handleClick}
@@ -54,9 +55,12 @@ export default function DeployButton() {
         <Tooltip
           className="z-20"
           anchorId={tooltipAnchorID}
-          content={canPush.reason}
+          content={canPush?.reason}
         />
       )}
     </button>
+    <span>GIT DATA: {JSON.stringify(canPush)}</span>
+    <span>hasChanges: {String(hasChanges)}</span>
+    </div>
   );
 }
