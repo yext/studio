1966b8de2a4f65c4127dc73780bc062d4d17a6e8
Tom Meyer <tmeyer@yext.com>
2023-10-05 10:40:17 -0400

    Targeted screenshots.

--- e2e-tests/tests/hmr/remove-component.spec.ts
+++ e2e-tests/tests/hmr/remove-component.spec.ts
@@ -3,9 +3,12 @@ import { studioTest } from "../infra/studioTest.js";
 import fs from "fs";
 
 studioTest("HMR for removing a component", async ({ studioPage }) => {
-  const addElementLocator =
-    studioPage.componentTreeSection.getElement("Banner");
+  const componentTreeSection = studioPage.componentTreeSection;
+  const addElementLocator = componentTreeSection.getElement("Banner");
   await expect(addElementLocator).toHaveCount(1);
+  await componentTreeSection.takeScreenshot();
+
   fs.rmSync(studioPage.getComponentPath("Banner"));
   await expect(addElementLocator).toHaveCount(0);
+  await componentTreeSection.takeScreenshot();
 });
