18387914088b1ec19628b892e4efd02ac03b81cb
Tom Meyer <tmeyer@yext.com>
2023-10-10 12:58:02 -0400

    Address more feedback.

--- packages/studio-ui/src/components/common/TooltipIcon.tsx
+++ packages/studio-ui/src/components/common/TooltipIcon.tsx
@@ -1,24 +1,20 @@
-import { CSSProperties, useMemo } from "react";
+import { useMemo } from "react";
 import { Tooltip } from "react-tooltip";
 import { v4 } from "uuid";
 import { ReactComponent as Info } from "../../icons/info.svg";
 
 export interface TooltipIconProps {
   content: string;
-  styles?: CSSProperties;
 }
 
-export default function TooltipIcon({ content, styles }: TooltipIconProps) {
+export default function TooltipIcon({ content }: TooltipIconProps) {
   const tooltipId = useMemo(() => v4(), []);
-  const defaultStyles = useMemo(() => {
-    return { backgroundColor: "black", zIndex: 1 };
-  }, []);
 
   return (
     <div>
       <Info id={tooltipId} className="ml-3 pb-1" data-testid="prop-tooltip" />
       <Tooltip
-        style={styles || defaultStyles}
+        className="bg-black z-20"
         anchorId={tooltipId}
         content={content}
         place="left"
