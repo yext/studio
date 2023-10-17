04ad4518d100b2f37011cb7715ab04d3c6946b1a
Alex Taing <ataing@yext.com>
2023-10-17 08:46:51 -0400

    fix test

--- e2e-tests/tests/collapse-sidebars.spec.ts
+++ e2e-tests/tests/collapse-sidebars.spec.ts
@@ -1,8 +1,8 @@
 import { studioTest } from "./infra/studioTest.js";
 
 studioTest("can-collapse-sidebars", async ({ page, studioPage }) => {
-  const leftSidebar = page.getByLabel("Collapse left sidebar");
-  const rightSidebar = page.getByLabel("Collapse right sidebar");
+  const leftSidebar = page.getByLabel("Collapse/Expand left sidebar");
+  const rightSidebar = page.getByLabel("Collapse/Expand right sidebar");
   await leftSidebar.click();
   await rightSidebar.click();
   await studioPage.takePageScreenshotAfterImgRender();
