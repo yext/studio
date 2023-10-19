ec6dde9ac04c30b7006675eb42d5b017051e0379
Alex Taing <ataing@yext.com>
2023-10-19 08:52:44 -0400

    no duplicate style in iFrame

--- packages/studio-ui/src/hooks/useInjectUserStyles.tsx
+++ packages/studio-ui/src/hooks/useInjectUserStyles.tsx
@@ -52,10 +52,11 @@ function clearStylingFromIframe(iframeDocument: Document) {
 }
 
 function injectStyleIntoIframe(iframeDocument: Document, filepath: string) {
-  const originalStyletag = document.querySelector(
-    `[studio-style-filepath='${filepath}']`
-  );
-  if (originalStyletag) {
+  const styletagIdAttribute = `[studio-style-filepath='${filepath}']`;
+  const originalStyletag = document.querySelector(styletagIdAttribute);
+  const oldIframeStyletag = iframeDocument.querySelector(styletagIdAttribute);
+  
+  if (originalStyletag && !oldIframeStyletag) {
     const iframeStyletag = originalStyletag.cloneNode(true);
     iframeDocument.head.appendChild(iframeStyletag);
   }
