3eed6c59bb94912a6595566dd2d7c0ddc20ae82d
Alex Taing <ataing@yext.com>
2023-10-25 09:47:38 -0400

    add tailwind

--- apps/test-site/src/templates/index.tsx
+++ apps/test-site/src/templates/index.tsx
@@ -1,5 +1,6 @@
 import { GetPath, TemplateProps } from "@yext/pages";
 import Button from "../components/Button";
+import "../styles/main.css";
 
 export const getPath: GetPath<TemplateProps> = () => {
   return "index.html";
